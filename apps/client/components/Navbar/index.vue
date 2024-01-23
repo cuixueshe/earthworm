425cffd0963325703932d6efe1f2c811cbca9edc
nauxscript <nauxscript@gmail.com>
2024-01-23 16:41:37 +0800

    feat: dark mode

--- apps/client/components/Navbar/index.vue
+++ apps/client/components/Navbar/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+
+const toggleDarkMode = () => {
+  document.documentElement.classList.toggle('dark')
+}
+
+</script>
+
 <template>
   <nav class="h-20 flex items-center justify-between">
     <NuxtLink to="/">
@@ -12,7 +20,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
       </button>
-      <button class="btn btn-sm btn-ghost rounded-md mx-1 w-8 h-8 p-0">
+      <button class="btn btn-sm btn-ghost rounded-md mx-1 w-8 h-8 p-0" @click="toggleDarkMode">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
         </svg>
