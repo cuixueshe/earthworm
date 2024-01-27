9570666943cba76e2e70a3d59b3dc994ba5c6461
nauxscript <nauxscript@gmail.com>
2024-01-27 10:35:40 +0800

    feat: daily sentence api

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { CourseModule } from '../course/course.module';
 import { UserProgressModule } from '../user-progress/user-progress.module';
+import { ToolModule } from 'src/tool/tool.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserProgressModule } from '../user-progress/user-progress.module';
     AuthModule,
     CourseModule,
     UserProgressModule,
+    ToolModule,
   ],
 })
 export class AppModule {}
