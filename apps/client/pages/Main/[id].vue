2f0f8fdcace1f120617052da4be4e13091630431
hildxd <hildxd@outlook.com>
2024-01-27 16:16:15 +0800

    feat: progress rank base

--- apps/client/pages/Main/[id].vue
+++ apps/client/pages/Main/[id].vue
@@ -2,12 +2,14 @@
   <div class="relative">
     <Game></Game>
     <Tool></Tool>
+    <ProgressRank />
   </div>
 </template>
 
 <script setup lang="ts">
 import Game from "~/components/main/Game.vue";
 import Tool from '~/components/main/Tool.vue';
+import ProgressRank from '~/components/rank/ProgressRank.vue';
 import { useCourseStore } from "~/store/course";
 
 const route = useRoute();
