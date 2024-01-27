f74563c3ecc458fe58d13e5e67a7bd8bfc2b1461
nauxscript <nauxscript@gmail.com>
2024-01-27 11:59:17 +0800

    fix: progress body error

--- apps/api/src/user-progress/model/user-progress.dto.ts
+++ apps/api/src/user-progress/model/user-progress.dto.ts
@@ -6,3 +6,9 @@ export class CreateUserProgressDto {
   @IsNotEmpty({ message: '课程不能为空' })
   courseId: number;
 }
+
+export class UpdateUserProgressDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: '课程不能为空' })
+  courseId: number;
+}
