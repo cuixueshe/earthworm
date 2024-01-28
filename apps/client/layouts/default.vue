b62f3a5fee24da27260e8cdb5cbd6c1fd3661c63
nauxscript <nauxscript@gmail.com>
2024-01-28 23:04:44 +0800

    fix: layout height

--- apps/client/layouts/default.vue
+++ apps/client/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full bg-white dark:bg-slate-900 transition-colors dark:text-slate-300 text-slate-600">
-    <div class="flex flex-col h-screen xl:w-[1200px] m-auto xl:px-2 px-24">
+    <div class="flex flex-col h-fit min-h-screen xl:w-[1200px] m-auto xl:px-2 px-24">
       <Navbar></Navbar>
       <div class="flex-1 flex flex-col">
         <slot></slot>
