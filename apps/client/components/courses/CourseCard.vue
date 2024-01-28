bea6899acfc99112e712bc32fda4bdac22c704a5
fengstats <feng2860984180@163.com>
2024-01-29 07:10:34 +0800

    feat: responsive layout for course list

--- apps/client/components/courses/CourseCard.vue
+++ apps/client/components/courses/CourseCard.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="w-[330px] h-[220px] flex flex-col cursor-pointer border border-slate-400 rounded-xl px-6 py-4 hover:shadow-lg hover:shadow-purple-400/50 hover:border-purple-400 transition-all mb-4">
-    <p class="text-2xl leading-loose font-bold">{{ title }}</p>
-    <div class="mb-4"></div>
-    <div>this is the course's description</div>
+  <div class="
+        flex
+        flex-col
+        w-[360px]
+        h-[160px]
+        sm:w-[500px]
+        md:w-[340px]
+        lg:w-[280px]
+        xl:w-[260px]
+        p-4 
+        pb-6
+        border
+        border-slate-400
+        rounded-xl
+        hover:shadow-lg
+        hover:shadow-purple-400/50
+        hover:border-purple-400
+        transition-all
+        duration-500
+  ">
+    <h3 class="text-xl font-bold dark:text-gray-100">{{ title }}</h3>
+    <p class="mt-4 truncate">this is the course's description</p>
   </div>
 </template>
 
