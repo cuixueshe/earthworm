643fa6419ddd91c423c13e584a7e87c2a3cc9803
cuixiaorui <cui_xiaorui@126.com>
2024-01-28 23:59:24 +0800

    feat: add prod command

---
+++ apps/api/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'earthworm_api',
+      script: 'pnpm',
+      args: 'start:prod',
+      cwd: '.',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
