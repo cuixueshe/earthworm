2daf22d11a4ea6265852a83354f81f3b02728474
myltx <summmayl@gmail.com>
2024-01-30 21:48:50 +0800

    fix: GameMode not reset error when switching courses

--- apps/client/middleware/auth.ts
+++ apps/client/middleware/auth.ts
@@ -1,6 +1,9 @@
+import { useGameMode } from "~/composables/main/game";
 import { useUserStore } from "~/store/user"
 
 export default defineNuxtRouteMiddleware((to, from) => {
+  const { resetGameMode } = useGameMode();
+  resetGameMode()
   const userStore = useUserStore()
   if (!userStore.getUserInfo() && +to.params.id !== 1) {
     return navigateTo("/auth/login");
