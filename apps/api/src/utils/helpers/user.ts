08c4675bd41a5a8827b43d5351ec9517a423c363
hildxd <hildxd@outlook.com>
2024-02-01 21:17:58 +0800

    feat: update create mock user function

--- apps/api/src/utils/helpers/user.ts
+++ apps/api/src/utils/helpers/user.ts
@@ -1,11 +1,10 @@
-import { user } from '@earthworm/shared';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from '../../auth/auth.service';
 import { UserService } from '../../user/user.service';
-import { cleanupMockDb } from './cleanupDb';
 import { mockDb } from './mockDb';
 
 // only use this in test
+// 测试是并行的，phone 不能重复
 export async function createSignInfo() {
   let userService = new UserService(mockDb);
   let jwtService = new JwtService({
@@ -15,9 +14,18 @@ export async function createSignInfo() {
   let authService = new AuthService(userService, jwtService);
   const newUser = {
     name: 'test',
-    phone: '1550211222',
+    phone: generateRandomPhone(),
     password: 'password',
   };
   const res = await authService.signup(newUser);
   return res;
+}
+
+// 随机生成 11 位数的手机号
+function generateRandomPhone(): string {
+  let phone = '1'; // 手机号以1开头
+  for (let i = 0; i < 10; i++) {
+    phone += Math.floor(Math.random() * 10); // 生成0-9的随机数
+  }
+  return phone;
 }
