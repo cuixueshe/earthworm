5f6a21d49d52f1afb9a3d86b8a0c80a21c1cc8ea
hildxd <hildxd@outlook.com>
2024-02-01 17:02:15 +0800

    feat: add tool controller  test

---
+++ apps/api/src/tool/tool.controller.spec.ts
@@ -0,0 +1,37 @@
+import { ToolController } from './tool.controller';
+import { ToolService } from './tool.service';
+import * as superagent from 'superagent';
+
+jest.mock('superagent');
+
+describe.only('Tool', () => {
+  let toolController: ToolController;
+  let toolService: ToolService;
+
+  beforeAll(() => {
+    toolService = new ToolService();
+    toolController = new ToolController(toolService);
+  });
+
+  it('happy path', () => {
+    expect(true).toBe(true);
+  });
+  it('should return dailySentence', async () => {
+    jest.spyOn(superagent, 'get').mockResolvedValueOnce({
+      text: '{"note": "中文", "content": "english"}',
+    });
+    const data = await toolController.dailySentence();
+    expect(data).toMatchObject({
+      zh: '中文',
+      en: 'english',
+    });
+  });
+
+  it('should return null when dailySentence has no data', async () => {
+    jest.spyOn(superagent, 'get').mockResolvedValueOnce({
+      text: null,
+    });
+    const data = await toolController.dailySentence();
+    expect(data).toBeNull();
+  });
+});
