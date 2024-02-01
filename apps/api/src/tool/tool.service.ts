d9dae79c282558f09ae4f7a952366b9a857798d5
hildxd <hildxd@outlook.com>
2024-02-01 17:01:55 +0800

    refactor: when dailySentence get data is null

--- apps/api/src/tool/tool.service.ts
+++ apps/api/src/tool/tool.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import * as superagent from 'superagent';
 
+function hasOwnProperty(obj, key) {
+  return Object.prototype.hasOwnProperty.call(obj, key);
+}
+
 @Injectable()
 export class ToolService {
   async dailySentence() {
     // refer to https://open.iciba.com/index.php?c=wiki
     const { text } = await superagent.get('https://open.iciba.com/dsapi/');
-    const data = JSON.parse(text);
-    const res = {
-      zh: data.note,
-      en: data.content,
-    };
-    return res;
+    const data = JSON.parse(text || '{}');
+    if (hasOwnProperty(data, 'note') && hasOwnProperty(data, 'content')) {
+      const res = {
+        zh: data.note,
+        en: data.content,
+      };
+      return res;
+    }
+    return null;
   }
 }
