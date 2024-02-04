302024aba6fe27e5ca299daff96cde814ebf4cf6
jiushi21 <zc931325671@gmail.com>
2024-02-04 11:59:21 +0800

    refactor: Login and SignUp remove-naive-ui-use-tailwind-vee-validate

--- apps/client/pages/Auth/Login.vue
+++ apps/client/pages/Auth/Login.vue
@@ -1,41 +1,46 @@
 <template>
-  <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div
+    className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <img className="mx-auto h-10 w-auto" src="/logo.png" alt="earthworm" />
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300">
+      <h2
+        className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300"
+      >
         Log in to your account
       </h2>
     </div>
 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form @submit.prevent="handleLogin" class="space-y-6" novalidate>
-        <div>
-          <label for="phone" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">Phone <span
-              class="text-red-500">*</span></label>
-          <input v-model="phone" id="phone" name="phone" type="tel" required placeholder="Please enter your phone number"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600"
-            autocomplete="off">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.phone }}</p>
-        </div>
-        <div>
-          <label for="password" class="block mt-4 text-sm font-medium text-gray-700 dark:text-gray-300">Password <span
-              class="text-red-500">*</span></label>
-          <input v-model="password" id="password" name="password" type="password" required
-            placeholder="Please enter your password"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.password }}</p>
-        </div>
-        <div class="pt-2">
-          <button type="submit"
-            class="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Log in
-          </button>
-        </div>
-      </form>
+      <n-form ref="formEl" :rules="rules" :model="model">
+        <n-form-item path="phone" label="Phone" required>
+          <n-input v-model:value="model.phone" @keydown.enter.prevent @keyup.enter="handleLogin"/>
+        </n-form-item>
+        <n-form-item path="password" label="Password" required>
+          <n-input
+            v-model:value="model.password"
+            type="password"
+            @keydown.enter.prevent
+            @keyup.enter="handleLogin"
+          />
+        </n-form-item>
+        <n-button
+          type="primary"
+          size="large"
+          block
+          @click="handleLogin"
+          class="mt-2"
+        >
+          Log in
+        </n-button>
+      </n-form>
+
       <p className="mt-10 text-center text-sm text-gray-500">
         Not an account?
-        <NuxtLink href="/auth/signup"
-          className="font-semibold text-[1.2em] leading-6 text-fuchsia-500 hover:text-fuchsia-400">
+        <NuxtLink
+          href="/auth/signup"
+          className="font-semibold text-[1.2em] leading-6 text-fuchsia-500 hover:text-fuchsia-400"
+        >
           Sign up
         </NuxtLink>
       </p>
@@ -43,34 +48,62 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useForm, useField } from 'vee-validate';
-import * as yup from 'yup';
+import { type FormInst, type FormRules } from "naive-ui";
 import { useAuth } from "~/composables/auth";
-import { useRouter, useRoute } from 'vue-router';
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useMessage } from "naive-ui";
 
-const schema = yup.object({
-  phone: yup.string().required("请输入您的手机号码").matches(/^1[3456789]\d{9}$/, "请输入正确的手机号码"),
-  password: yup.string().required("请输入您的密码").min(6, "密码长度必须大于6"),
-});
+interface ModelType {
+  phone: string | null;
+  password: string | null;
+}
 
-const { handleSubmit, errors } = useForm({
-  validationSchema: schema,
-});
-
-const { value: phone } = useField('phone');
-const { value: password } = useField('password');
-
-const router = useRouter();
-const route = useRoute();
 const { login } = useAuth();
 
-const handleLogin = handleSubmit(async (values) => {
-  try {
-    await login(values as { phone: string; password: string; });
-    router.replace(route.query.callback?.toString() || '/');
-  } catch (error) {
-    
-  }
+const formEl = ref<FormInst | null>(null);
+const model = ref<ModelType>({
+  phone: null,
+  password: null,
 });
 
+const rules: FormRules = {
+  phone: [
+    { required: true, message: "Please input your phone number" },
+    {
+      pattern: /^1[3456789]\d{9}$/,
+      message: "Please input correct phone number",
+    },
+  ],
+  password: [
+    { required: true, message: "Please input your password" },
+    { min: 6, message: "Password length must be greater than 6" },
+  ],
+};
+
+const message = useMessage();
+const router = useRouter();
+const route = useRoute();
+
+const handleLogin = () => {
+  function gotoPreviousPage() {
+    router.replace(route.query.callback?.toString() ?? "/");
+  }
+
+  formEl.value?.validate(async (errors) => {
+    if (!errors) {
+      await login({
+        phone: model.value.phone ?? "",
+        password: model.value.password ?? "",
+      });
+
+      message.success("login success", {
+        duration: 500,
+        onLeave() {
+          gotoPreviousPage();
+        },
+      });
+    }
+  });
+};
 </script>
--- apps/client/pages/Auth/SignUp.vue
+++ apps/client/pages/Auth/SignUp.vue
@@ -1,58 +1,57 @@
 <template>
-  <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div
+    className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <img className="mx-auto h-10 w-auto" src="/logo.png" alt="earthworm" />
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300">
+      <h2
+        className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300"
+      >
         Sign up to your account
       </h2>
     </div>
 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form @submit.prevent="handleRegister" class="space-y-6" novalidate>
-        <div>
-          <label for="name" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">Name <span
-              class="text-red-500">*</span></label>
-          <input v-model="name" id="name" name="name" type="text" required placeholder="Your name"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600"
-            autocomplete="off">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.name }}</p>
-        </div>
-        <div>
-          <label for="phone" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">Phone <span
-              class="text-red-500">*</span></label>
-          <input v-model="phone" id="phone" name="phone" type="tel" required placeholder="Your phone number"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600"
-            autocomplete="off">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.phone }}</p>
-        </div>
-        <div>
-          <label for="password" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">Password <span
-              class="text-red-500">*</span></label>
-          <input v-model="password" id="password" name="password" type="password" required placeholder="Your password"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600"
-            autocomplete="off">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.password }}</p>
-        </div>
-        <div>
-          <label for="confirmPassword" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">Confirm
-            Password <span class="text-red-500">*</span></label>
-          <input v-model="confirmPassword" id="confirmPassword" name="confirmPassword" type="password" required
-            placeholder="Confirm your password"
-            class="mt-2 appearance-none block w-full px-3 py-1.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-sm text-gray-700 dark:text-gray-300 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-600 dark:bg-gray-700 focus:bg-blue-50 dark:focus:bg-gray-600"
-            autocomplete="off">
-          <p class="text-red-500 text-opacity-80 text-xs h-1 m-1">{{ errors.confirmPassword }}</p>
-        </div>
-        <div class="pt-2">
-          <button type="submit"
-            class="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Sign up
-          </button>
-        </div>
-      </form>
+      <n-form ref="formEl" :rules="rules" :model="model">
+        <n-form-item path="name" label="Name" required>
+          <n-input v-model:value="model.name" @keydown.enter.prevent @keyup.enter="handleRegister"/>
+        </n-form-item>
+        <n-form-item path="phone" label="Phone" required>
+          <n-input v-model:value="model.phone" @keydown.enter.prevent @keyup.enter="handleRegister"/>
+        </n-form-item>
+        <n-form-item path="password" label="Password" required>
+          <n-input
+            v-model:value="model.password"
+            type="password"
+            @keydown.enter.prevent
+            @keyup.enter="handleRegister"
+          />
+        </n-form-item>
+        <n-form-item path="confirmPassword" label="ConfirmPassword" required>
+          <n-input
+            v-model:value="model.confirmPassword"
+            type="password"
+            @keydown.enter.prevent
+            @keyup.enter="handleRegister"
+          />
+        </n-form-item>
+        <n-button
+          type="primary"
+          size="large"
+          block
+          @click="handleRegister"
+          class="mt-2"
+        >
+          Sign in
+        </n-button>
+      </n-form>
+
       <p className="mt-10 text-center text-sm text-gray-500 dark:text-gray-400">
         Has an account?
-        <NuxtLink href="/auth/login"
-          className="font-semibold text-[1.2em] leading-6 text-fuchsia-500 hover:text-fuchsia-400">
+        <NuxtLink
+          href="/auth/login"
+          className="font-semibold text-[1.2em] leading-6 text-fuchsia-500 hover:text-fuchsia-400"
+        >
           Log in
         </NuxtLink>
       </p>
@@ -60,44 +59,77 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useForm, useField } from 'vee-validate';
-import * as yup from 'yup';
+import { type FormInst, type FormRules, type FormItemRule } from "naive-ui";
 import { useAuth } from "~/composables/auth";
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useMessage } from "naive-ui";
 
-const schema = yup.object({
-  name: yup.string().required("Please enter your name").min(2, "Name must be at least 2 characters").max(20, "Name must be less than 20 characters"),
-  phone: yup.string().required("Please enter your phone number").matches(/^1[3456789]\d{9}$/, "Please enter a valid phone number"),
-  password: yup.string().required("Please enter your password").min(6, "Password must be at least 6 characters"),
-  confirmPassword: yup.string().required("Please confirm your password").oneOf([yup.ref('password')], "Passwords must match"),
-});
+interface ModelType {
+  name: string | null;
+  phone: string | null;
+  password: string | null;
+  confirmPassword: string | null;
+}
 
-const { handleSubmit, errors } = useForm({
-  validationSchema: schema,
-});
-
-const { value: name } = useField('name');
-const { value: phone } = useField('phone');
-const { value: password } = useField('password');
-const { value: confirmPassword } = useField('confirmPassword');
-
-const router = useRouter();
 const { signup } = useAuth();
+const formEl = ref<FormInst | null>(null);
 
-const handleRegister = handleSubmit(async (values) => {
-  try {
-    const signupValues = {
-      phone: values.phone as string,
-      name: values.name as string,
-      password: values.password as string,
-    };
-    await signup(signupValues);
-    router.replace("/");
-  } catch (error) {
-    
-  }
+const model = ref<ModelType>({
+  name: null,
+  phone: null,
+  password: null,
+  confirmPassword: null,
 });
 
+function validatePasswordSame(rule: FormItemRule, value: string): boolean {
+  return value === model.value.password;
+}
+
+const rules: FormRules = {
+  name: [
+    { required: true, message: "Please input you name" },
+    { min: 2, max: 20, message: "name length must be min 2 and max 20" },
+  ],
+  phone: [
+    { required: true, message: "Please input your phone number" },
+    {
+      pattern: /^1[3456789]\d{9}$/,
+      message: "Please input correct phone number",
+    },
+  ],
+  password: [
+    { required: true, message: "Please input your password" },
+    { min: 6, max: 20, message: "Password length must be greater than 6" },
+  ],
+  confirmPassword: [
+    { required: true, message: "Please input confirmPassword" },
+    {
+      validator: validatePasswordSame,
+      message: "The two password inputs are inconsistent",
+    },
+  ],
+};
+
+const message = useMessage();
+const router = useRouter();
+
+const handleRegister = () => {
+  formEl.value?.validate(async (errors) => {
+    if (!errors) {
+      await signup({
+        phone: model.value.phone ?? "",
+        name: model.value.name ?? "",
+        password: model.value.password ?? "",
+      });
+
+      message.success("register success", {
+        duration: 500,
+        onLeave() {
+          router.replace("/");
+        },
+      });
+    }
+  });
+};
 </script>
-
-
