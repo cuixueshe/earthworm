c613af58dc136a7dfc62c81e6ddba95a7dfe6ca8
zwd <17326195691@163.com>
2024-02-05 14:40:49 +0800

    chore: add commit rules

---
+++ .releaserc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  branches: ["chore/commitRule"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+  ],
+};
