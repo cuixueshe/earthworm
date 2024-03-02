6d14039b89daad542ee13ae28f3e13d2255d907a
nauxscript <nauxscript@gmail.com>
2024-03-02 12:11:36 +0800

    feat: One-to-one corresponding course links

--- apps/client/components/main/StudyVideoLink.vue
+++ apps/client/components/main/StudyVideoLink.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink class="tooltip text-xl cursor-pointer fill-current hover:text-[#d946ef]" :data-tip="NOTE_TIP"
-    :href="NOTE_LINK" target="_blank">
+    :href="noteLink" target="_blank">
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
       <path
         d="M7.172 2.757L10.414 6h3.171l3.243-3.242a1 1 0 1 1 1.415 1.415L16.414 6H18.5A3.5 3.5 0 0 1 22 9.5v8a3.5 3.5 0 0 1-3.5 3.5h-13A3.5 3.5 0 0 1 2 17.5v-8A3.5 3.5 0 0 1 5.5 6h2.085L5.757 4.171a1 1 0 0 1 1.415-1.415M18.5 8h-13a1.5 1.5 0 0 0-1.493 1.356L4 9.5v8a1.5 1.5 0 0 0 1.356 1.493L5.5 19h13a1.5 1.5 0 0 0 1.493-1.355L20 17.5v-8A1.5 1.5 0 0 0 18.5 8M8 11a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1m8 0a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1" />
@@ -8,6 +8,18 @@
   </NuxtLink>
 </template>
 <script setup lang="ts">
+import { getCourseLinks } from '~/utils/courseLinks'
+import { computed, defineProps } from 'vue';
+
 const NOTE_TIP = '边学边练: 点击跳转至星荣英语笔记'
-const NOTE_LINK = 'https://www.bilibili.com/video/BV13g411F7Kd';
+const props = defineProps<{
+  courseId?: number
+}>()
+
+const noteLinks = getCourseLinks()
+
+const noteLink = computed(() => {
+  return noteLinks[props.courseId || 0]
+})
+
 </script>
---
+++ apps/client/utils/courseLinks.ts
@@ -0,0 +1,23 @@
+import courseLinks from '~/assets/courseLinks.json';
+
+// 星荣课程列表
+// 由于改版，现在无法获取到课程列表
+// 需要手动在浏览器调用接口获取数据
+// 返回的数据结构是:
+// {
+//     code: 0,
+//     message: "0",
+//     data: {
+//         aids: [...],
+//         archives: [...],
+//         meta: {...},
+//         page: {...}
+//     }
+// }
+// 我们需要获取的是 data.archives
+// 把其放到 assets/courseLinks.json 中
+// const url = 'https://api.bilibili.com/x/polymer/web-space/seasons_archives_list?mid=160507280&season_id=48449&page_num=1&page_size=99'
+
+const prefix = 'https://www.bilibili.com/video/';
+
+export const getCourseLinks = () => courseLinks.map((courseLink) => `${prefix}${courseLink.bvid}`);
