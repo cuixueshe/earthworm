defaf19d700937193b28541e1e84510d404b3dd6
fengstats <feng2860984180@163.com>
2024-03-12 20:28:53 +0800

    style: added homepage quick start introduction

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -4,30 +4,43 @@
       <Loading></Loading>
     </template>
     <template v-else>
-      <section class="flex md:flex-row md:justify-between justify-center flex-col py-8">
+      <section
+        class="flex md:flex-row md:justify-between justify-center flex-col py-8"
+      >
         <div class="w-1/2 mx-4">
           <div class="mb-12 leading-loose text-3xl opacity-80 items-center">
             <div class="">Why aren’t you good at English?</div>
             <div class="align-middle">
               It’s because you haven’t used
               <span class="text-fuchsia-400 font-bold">EARTHWORM</span> yet! 🤪
-              <i class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-2xl p-[2px]"></i>
+              <i
+                class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-2xl p-[2px]"
+              ></i>
             </div>
           </div>
-          <a class="mr-4 btn w-48 indicator" target="_blank" href="https://github.com/cuixueshe/earthworm">
-              <span class="indicator-item">🌟</span>
-              Star us on GitHub
+          <a
+            class="mr-4 btn w-48 indicator"
+            target="_blank"
+            href="https://github.com/cuixueshe/earthworm"
+          >
+            <span class="indicator-item">🌟</span>
+            Star us on GitHub
           </a>
-          <button @click="handleKeydown"
-            class="btn btn-outline w-48 hover:text-fuchsia-400 hover:border-fuchsia-400 hover:bg-fuchsia-100 text-fuchsia-300 border-fuchsia-300">
+          <button
+            @click="handleKeydown"
+            class="btn btn-outline w-48 hover:text-fuchsia-400 hover:border-fuchsia-400 hover:bg-fuchsia-100 text-fuchsia-300 border-fuchsia-300"
+          >
             Get Started<kbd class="kbd"> ↵ </kbd>
           </button>
         </div>
-        <div class="w-1/2 flex items-center justify-center group select-none cursor-pointer rounded-xl relative m-4">
+        <div
+          class="w-1/2 flex items-center justify-center group select-none cursor-pointer rounded-xl relative m-4"
+        >
           <div class="absolute flex h-full w-full card">
             <div class="bg-dot rounded-[64px]"></div>
             <div
-              class="absolute left-0 right-0 top-0 text-[220px] text-center group-hover:-skew-y-12 group-hover:rotate-12 transition-all">
+              class="absolute left-0 right-0 top-0 text-[220px] text-center group-hover:-skew-y-12 group-hover:rotate-12 transition-all"
+            >
               📖
             </div>
             <!-- <div
@@ -44,64 +57,103 @@
         </p>
         <div class="flex">
           <div
-            class="rounded-3xl my-8 mx-2 border dark:border-slate-600 bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-600/90 dark:to-neutral-450/90 w-1/2 hover:shadow-2xl">
+            class="rounded-3xl my-8 mx-2 border dark:border-slate-600 bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-600/90 dark:to-neutral-450/90 w-1/2 hover:shadow-2xl"
+          >
             <div class="h-[330px] flex flex-col pt-4 pl-4 pb-4 text-sm">
-              <div class="overflow-y-auto">
-                <h2 class="text-xl font-bold py-4">快速上手</h2>
+              <div class="pr-2 overflow-y-auto">
+                <h2 class="py-4 text-xl font-bold">快速上手</h2>
+                <h3 class="pb-2 text-base font-semibold">当前页面</h3>
                 <p class="py-1">
-                  当前页面，点击按钮
+                  点击按钮
                   <span
-                    class="inline-block px-1 text-fuchsia-300 border border-solid border-fuchsia-300 rounded-lg"
+                    class="inline-block mx-1 px-1 text-fuchsia-300 border border-solid border-fuchsia-300 rounded-lg"
                     >Get Started</span
                   >
-                  <span class="text-gray-500"> (或快捷键 Enter)</span> 开启你的第一节课。
+                  <span class="text-gray-500">（或快捷键 Enter ⏎）</span>
+                  开启你的第一节课！
                 </p>
-                <p class="py-1">
-                  课程页面，紫色下划线为聚焦状态，输入你的答案，敲击回车键验证结果。
+                <h3 class="py-2 text-base font-semibold">答题页面</h3>
+                <p class="py-1 leading-7">
+                  看到
+                  <span class="text-fuchsia-500">紫色下划线</span>
+                  亮起，输入单词，按下空格键
+                  <span class="text-gray-500">（Space ␣）</span>
+                  向后切换单词，待输入完成后再按下回车键
+                  <span class="text-gray-500">（Enter ⏎）</span>
+                  来提交题目
                 </p>
-                <p class="py-1">验证通过才会显示正确结果，并语音播报。</p>
+                <p class="py-1 leading-7">
+                  题目提交后若出现
+                  <span class="text-red-500">红色</span>
+                  单词和下划线显示，代表这部分单词存在某些错误需要更正，此时可按下空格键
+                  <span class="text-gray-500">（Space ␣）</span>
+                  快速定位到第一个错误单词并清空，完成更改后再按一次空格继续定位到下个错词
+                </p>
+                <p class="py-1 leading-7">
+                  此时若发现上个错词未正确修改，可按下退格键
+                  <span class="text-gray-500">（Backspace ⌫）</span>
+                  来回退到上一个错词，直到所有错词被更正完成，按下回车键提交即可
+                </p>
+                <p class="py-1 leading-7">
+                  验证通过后会显示答案页面，同时会播放对应题目语音用于辅助记忆。
+                </p>
+                <h3 class="py-2 text-base font-semibold">答题小技巧</h3>
+                <p class="py-1 leading-7">
+                  底部提示面板的按钮
+                  <a
+                    class="ml-1 text-[#3498db] hover:text-fuchsia-500"
+                    href="https://www.bilibili.com/video/BV1py421q7Mp/"
+                  >
+                    👉 一分钟点我快速了解</a
+                  >
+                </p>
+                <p class="p-1">
+                  <button class="instruction-btn">⌃ Ctrl+'</button>
+                  <span class="text-gray-500">（或快捷键 Ctrl+'）</span>
+                  播放题目语音
+                </p>
+                <p class="p-1">
+                  <button class="instruction-btn">⌃ Ctrl+;</button>
+                  <span class="text-gray-500">（或快捷键 Ctrl+;）</span>
+                  显示题目答案
+                </p>
+                <h3 class="py-2 text-base font-semibold">答案页面</h3>
+                <p class="py-1 leading-7">答案下方的按钮</p>
                 <p class="py-1">
-                  点击按钮
                   <button class="instruction-btn">again</button>
-                  <span class="text-gray-500"> (或快捷键 Ctrl+')</span>
-                  重置此前操作，再来亿次怎能学不会。
+                  <span class="text-gray-500">（或快捷键 Ctrl+;）</span>
+                  再来亿次，怎能不会！
                 </p>
                 <p class="py-1">
-                  点击按钮
                   <button class="instruction-btn">next</button>
-                  <span class="text-gray-500">(或快捷键 Enter)</span>
-                  开始下一个，冲冲冲霸占榜首。
+                  <span class="text-gray-500">（或快捷键 Enter）</span>
+                  下一题，冲冲冲霸占榜首！🏄‍♂️
                 </p>
 
                 <h2 class="text-xl font-bold py-4">帮助</h2>
-                <p class="py-1">
-                  课程页面，输入下划线不是
-                  <span class="text-fuchsia-300">紫色</span>
-                  时无法输入，需要你动动小手移动鼠标点击聚焦即可。
+                <h3 class="pb-2 text-base font-semibold">课程页面</h3>
+                <p class="py-1 leading-7">
+                  输入下划线全是灰色，没有
+                  <span class="text-fuchsia-500">紫色</span>
+                  亮起时无法输入，需要你动动小手移动鼠标点击输入框进行聚焦哦~
                 </p>
-                <p class="py-1">输入状态下想要获取提示：</p>
-                <p class="p-1">
-                  1. 可以点击页面下方的按钮
-                  <button class="instruction-btn">⌃ Ctrl+'</button>
-                  <span class="text-gray-500">(或快捷键 Ctrl+')</span> 播放语音。
-                </p>
-                <p class="p-1">
-                  2. 也可以点击按钮
-                  <button class="instruction-btn">⌃ Ctrl+;</button>
-                  <span class="text-gray-500">(或快捷键 Ctrl+;)</span> 显示答案。
-                </p>
-                <p class="py-1">
+                <h3 class="py-2 text-base font-semibold">用户设置页面</h3>
+                <p class="py-1 leading-7">
                   右上角图标进入 User Info
-                  设置页面，可以自定义修改你喜欢的快捷键，也可以控制语音是否自动播放，更多个人设置会持续更新...
-                  <i class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-sm p-[2px]"></i>
+                  切换到设置页面，自定义你喜欢的快捷键，也可以控制语音是否自动播放、单词下划线固定长度、使用空格提交等等……更多个人设置会持续更新
+                  😊
+                  <i
+                    class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-sm p-[2px]"
+                  ></i>
                 </p>
               </div>
             </div>
           </div>
           <div
-            class="rounded-3xl my-8 mx-2 border dark:border-slate-600 bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-600/90 dark:to-neutral-450/90 w-1/2 hover:shadow-xl">
+            class="rounded-3xl my-8 mx-2 border dark:border-slate-600 bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-600/90 dark:to-neutral-450/90 w-1/2 hover:shadow-xl"
+          >
             <div class="h-[330px] flex p-4">
-              这里是 earthworm 背后的学习原理 写的好不好?
+              这里是 Earthworm 背后的学习原理……
             </div>
           </div>
         </div>
@@ -113,8 +165,12 @@
       </section>
     </template>
 
-    <MessageBox v-model:is-show-modal="showMobileTip" content="The app isn't mobile-friendly, so stay tuned!"
-      cancel-btn-text="fine" confirm-btn-text=""></MessageBox>
+    <MessageBox
+      v-model:is-show-modal="showMobileTip"
+      content="The app isn't mobile-friendly, so stay tuned!"
+      cancel-btn-text="fine"
+      confirm-btn-text=""
+    ></MessageBox>
   </div>
 </template>
 
@@ -193,9 +249,11 @@ function useShortcutToGame() {
   content: "";
   position: absolute;
   inset: 0;
-  background-image: radial-gradient(circle at center,
-      #a800b2 0.06rem,
-      transparent 0.65rem);
+  background-image: radial-gradient(
+    circle at center,
+    #a800b2 0.06rem,
+    transparent 0.65rem
+  );
   background-size: var(--bgSize, 1rem) var(--bgSize, 1rem);
   background-repeat: round;
   background-position:
