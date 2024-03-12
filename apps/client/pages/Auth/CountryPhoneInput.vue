a33af16062dcc045a9e2e5cd9c4a3d58b4f908c7
jiushi21 <zc931325671@gmail.com>
2024-03-12 22:28:04 +0800

    feat: Optimized the registration page-optimization of the drop-down box-mobile phone number verification logic-some other details

--- apps/client/pages/Auth/CountryPhoneInput.vue
+++ apps/client/pages/Auth/CountryPhoneInput.vue
@@ -7,17 +7,37 @@
       Phone <span class="text-red-500">*</span>
     </label>
     <div class="mt-2 flex rounded-md shadow-sm relative">
+      <div
+        @click="toggleDropdown"
+        tabindex="0"
+        class="rounded-l-md border border-gray-300 shadow-sm text-gray-700 dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:z-10 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 appearance-none block px-3 py-1.5 text-sm focus:outline-none cursor-pointer"
+      >
+        {{ selectedCountry.flag }} (+{{ selectedCountry.dialCode }})
+      </div>
+      <div
+        v-if="showDropdown"
+        class="absolute z-10 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 mt-10 rounded-md w-full max-h-80 overflow-auto"
+      >
+        <div
+          v-for="country in countries"
+          :key="country.iso2"
+          @click="selectCountry(country)"
+          class="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer"
+        >
+          {{ country.flag }} (+{{ country.dialCode }}) {{ country.name }}
+        </div>
+      </div>
       <select
-        v-model="selectedCountry"
+        v-model="selectedCountry.iso2"
         @change="emitUpdate"
-        class="rounded-l-md border border-gray-300 shadow-sm text-gray-700 dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:z-10 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 appearance-none block px-3 py-1.5 text-sm focus:outline-none"
+        class="hidden"
       >
         <option
           v-for="country in countries"
           :key="country.iso2"
           :value="country.iso2"
         >
-          {{ country.flag }} (+{{ country.dialCode }})
+          {{ country.flag }} (+{{ country.dialCode }}) {{ country.name }}
         </option>
       </select>
 
@@ -25,7 +45,7 @@
         <input
           type="tel"
           v-model="phoneNumber"
-          @input="emitUpdate"
+          @input="handleInput"
           placeholder="Your phone number"
           class="rounded-r-md border border-gray-300 shadow-sm text-gray-700 dark:text-gray-300 dark:border-gray-600 focus:outline-none dark:bg-gray-700 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 appearance-none block w-full px-3 py-1.5 text-sm placeholder-gray-400 dark:placeholder-gray-500 focus:bg-blue-50 dark:focus:bg-gray-600 -ml-px"
         />
@@ -38,7 +58,7 @@
 </template>
 
 <script setup>
-import { ref, computed, watch } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import { allCountries as countriesData } from "country-telephone-data";
 const emit = defineEmits(["update:modelValue"]);
 const props = defineProps({
@@ -46,33 +66,59 @@ const props = defineProps({
   errorMessage: String,
 });
 const countries = ref(
-  countriesData.map((country) => ({
-    ...country,
-    flag: countryCodesToFlagEmoji(country.iso2),
-  }))
+  countriesData
+    .sort((a, b) => (a.dialCode > b.dialCode ? 1 : -1))
+    .map((country) => ({
+      ...country,
+      flag: countryCodesToFlagEmoji(country.iso2),
+    }))
 );
-const selectedCountry = ref(countries.value[46].iso2);
+const defaultCountryIso2 = "cn";
+const defaultCountry = countries.value.find(
+  (country) => country.iso2 === defaultCountryIso2
+);
+const selectedCountry = ref(defaultCountry || {});
+
 const phoneNumber = ref("");
-const selectedCountryDialCode = computed(() => {
-  const country = countries.value.find((c) => c.iso2 === selectedCountry.value);
-  return country ? country.dialCode : "";
-});
-watch(
-  () => props.modelValue,
-  (newVal) => {
-    const justNumber = newVal
-      .replace(`+${selectedCountryDialCode.value}`, "")
-      .trim();
-    phoneNumber.value = justNumber;
-  }
-);
+const showDropdown = ref(false);
+
+function toggleDropdown() {
+  showDropdown.value = !showDropdown.value;
+}
+
+function selectCountry(country) {
+  selectedCountry.value = country;
+  showDropdown.value = false;
+  emitUpdate();
+}
+
+function handleInput(event) {
+  phoneNumber.value = event.target.value.replace(/\D/g, "");
+  emitUpdate();
+}
+
 function emitUpdate() {
-  const fullPhoneNumber = `+${selectedCountryDialCode.value}${phoneNumber.value}`;
+  const fullPhoneNumber = `+${selectedCountry.value.dialCode}_${phoneNumber.value}`;
   emit("update:modelValue", fullPhoneNumber);
 }
+
 function countryCodesToFlagEmoji(iso2) {
   return iso2
     .toUpperCase()
     .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397));
+}
+
+onMounted(() => {
+  document.addEventListener("click", handleClickOutside, true);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", handleClickOutside, true);
+});
+
+function handleClickOutside(event) {
+  if (!event.target.closest(".dropdown-container")) {
+    showDropdown.value = false;
+  }
 }
 </script>
--- apps/client/pages/Auth/SignUp.vue
+++ apps/client/pages/Auth/SignUp.vue
@@ -1,31 +1,71 @@
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-10 w-auto" src="/logo.png" alt="earthworm" />
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="/logo.png"
+        alt="earthworm"
+      />
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-300"
+      >
         Sign up to your account
       </h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
-      <form @submit.prevent="handleRegister" class="space-y-6" novalidate>
-        <FormInput label="Name" name="name" placeholder="Your name" v-model="name" :errorMessage="nameError" />
-        <CountryPhoneInput label="Phone" name="phone" v-model="phone" :errorMessage="phoneError"
-          @update:country="updateCountryCode" />
-        <FormInput label="Password" name="password" type="password" placeholder="Your password" v-model="password"
-          :errorMessage="passwordError" />
-        <FormInput label="Confirm Password" name="confirmPassword" type="password" placeholder="Confirm your password"
-          v-model="confirmPassword" :errorMessage="confirmPasswordError" />
+      <form
+        @submit.prevent="handleRegister"
+        class="space-y-6"
+        novalidate
+      >
+        <FormInput
+          label="Name"
+          name="name"
+          placeholder="Your name"
+          v-model="name"
+          :errorMessage="nameError"
+        />
+        <CountryPhoneInput
+          label="Phone"
+          name="phone"
+          v-model="phone"
+          :errorMessage="phoneError"
+          @update:country="updateCountryCode"
+        />
+        <FormInput
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Your password"
+          v-model="password"
+          :errorMessage="passwordError"
+        />
+        <FormInput
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm your password"
+          v-model="confirmPassword"
+          :errorMessage="confirmPasswordError"
+        />
         <div class="pt-2">
-          <button type="submit"
-            class="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+            type="submit"
+            class="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
             Sign up
           </button>
         </div>
       </form>
       <p class="mt-10 text-center text-sm text-gray-500 dark:text-gray-400">
         Already have an account?
-        <NuxtLink to="/auth/login" class="font-semibold text-indigo-600 hover:text-indigo-500">
+        <NuxtLink
+          to="/auth/login"
+          class="font-semibold text-indigo-600 hover:text-indigo-500"
+        >
           Log in
         </NuxtLink>
       </p>
@@ -38,19 +78,27 @@ import FormInput from "~/pages/Auth/FormInput.vue";
 import CountryPhoneInput from "~/pages/Auth/CountryPhoneInput.vue";
 import { useForm, useField } from "vee-validate";
 import * as yup from "yup";
-import {
-  type CountryCode,
-  parsePhoneNumberFromString,
-} from "libphonenumber-js";
 import { useAuth } from "~/composables/auth";
 import { useRouter } from "vue-router";
 import { type SignupFormValues } from "~/store/user";
-import { ref } from "vue";
+import { ref, type Ref } from "vue";
+import { PhoneNumberUtil } from "google-libphonenumber";
 
-const countryRef = ref("");
+const phoneUtil = PhoneNumberUtil.getInstance();
+const countryRef: Ref<string> = ref("");
 function updateCountryCode(code: string) {
   countryRef.value = code;
 }
+const phoneValidator = (phoneWithCode: string) => {
+  if (!phoneWithCode) return false;
+  const phoneNumberForValidation = phoneWithCode.replace("_", "");
+  try {
+    const number = phoneUtil.parse(phoneNumberForValidation, countryRef.value);
+    return phoneUtil.isValidNumber(number);
+  } catch (error) {
+    return false;
+  }
+};
 
 const schema = yup.object({
   name: yup
@@ -61,19 +109,17 @@ const schema = yup.object({
   phone: yup
     .string()
     .required("Please enter your phone number")
-    .test("is-valid-phone", "Please enter a valid phone number", (value) => {
-      if (!value) return false;
-      const phoneNumber = parsePhoneNumberFromString(
-        value,
-        countryRef.value as CountryCode
-      );
-      return phoneNumber ? phoneNumber.isValid() : false;
-    }),
+    .test(
+      "is-valid-phone",
+      "Please enter a valid phone number",
+      phoneValidator
+    ),
 
   password: yup
     .string()
     .required("Please enter your password")
-    .min(6, "Password must be at least 6 characters"),
+    .min(6, "Password must be at least 6 characters")
+    .max(20, "Password must be no more than 20 characters"),
   confirmPassword: yup
     .string()
     .required("Please confirm your password")
@@ -85,11 +131,7 @@ const { handleSubmit } = useForm<SignupFormValues>({
 });
 
 const { value: name, errorMessage: nameError } = useField<string>("name");
-const {
-  value: phone,
-  errorMessage: phoneError,
-  setValue: setPhoneValue,
-} = useField<string>("phone");
+const { value: phone, errorMessage: phoneError } = useField<string>("phone");
 const { value: password, errorMessage: passwordError } =
   useField<string>("password");
 const { value: confirmPassword, errorMessage: confirmPasswordError } =
@@ -99,17 +141,16 @@ const router = useRouter();
 const { signup } = useAuth();
 
 const handleRegister = handleSubmit(async (values) => {
-
-  const formattedPhone = values.phone.replace(/[^\d]/g, "");
+  //  countryCode :maybe backend api want to use it
+  const [countryCode, purePhoneNumber] = values.phone.split("_");
   const modifiedValues = {
     ...values,
-    phone: formattedPhone,
+    phone: purePhoneNumber,
   };
 
   try {
     await signup(modifiedValues);
     router.replace("/");
-  } catch (error) {
-  }
+  } catch (error) {}
 });
 </script>
