e7b34eef0bc9814986c20291f070a34ca9717fbd
mynameszhazh <1594776259@qq.com>
2024-03-12 14:30:07 +0800

    style: change global scrollbar style

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -45,53 +45,57 @@
         <div class="flex">
           <div
             class="rounded-3xl my-8 mx-2 border dark:border-slate-600 bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-600/90 dark:to-neutral-450/90 w-1/2 hover:shadow-2xl">
-            <div class="h-[330px] flex flex-col p-4 text-sm overflow-y-auto">
-              <h2 class="text-xl font-bold py-4">快速上手</h2>
-              <p class="py-1">
-                当前页面，点击按钮
-                <span class="inline-block px-1 text-fuchsia-300 border border-solid border-fuchsia-300 rounded-lg">Get
-                  Started</span>
-                <span class="text-gray-500"> (或快捷键 Enter)</span> 开启你的第一节课。
-              </p>
-              <p class="py-1">
-                课程页面，紫色下划线为聚焦状态，输入你的答案，敲击回车键验证结果。
-              </p>
-              <p class="py-1">验证通过才会显示正确结果，并语音播报。</p>
-              <p class="py-1">
-                点击按钮
-                <button class="instruction-btn">again</button>
-                <span class="text-gray-500"> (或快捷键 Ctrl+')</span>
-                重置此前操作，再来亿次怎能学不会。
-              </p>
-              <p class="py-1">
-                点击按钮
-                <button class="instruction-btn">next</button>
-                <span class="text-gray-500">(或快捷键 Enter)</span>
-                开始下一个，冲冲冲霸占榜首。
-              </p>
+            <div class="h-[330px] flex flex-col pt-4 pl-4 pb-4 text-sm">
+              <div class="overflow-y-auto">
+                <h2 class="text-xl font-bold py-4">快速上手</h2>
+                <p class="py-1">
+                  当前页面，点击按钮
+                  <span
+                    class="inline-block px-1 text-fuchsia-300 border border-solid border-fuchsia-300 rounded-lg"
+                    >Get Started</span
+                  >
+                  <span class="text-gray-500"> (或快捷键 Enter)</span> 开启你的第一节课。
+                </p>
+                <p class="py-1">
+                  课程页面，紫色下划线为聚焦状态，输入你的答案，敲击回车键验证结果。
+                </p>
+                <p class="py-1">验证通过才会显示正确结果，并语音播报。</p>
+                <p class="py-1">
+                  点击按钮
+                  <button class="instruction-btn">again</button>
+                  <span class="text-gray-500"> (或快捷键 Ctrl+')</span>
+                  重置此前操作，再来亿次怎能学不会。
+                </p>
+                <p class="py-1">
+                  点击按钮
+                  <button class="instruction-btn">next</button>
+                  <span class="text-gray-500">(或快捷键 Enter)</span>
+                  开始下一个，冲冲冲霸占榜首。
+                </p>
 
-              <h2 class="text-xl font-bold py-4">帮助</h2>
-              <p class="py-1">
-                课程页面，输入下划线不是
-                <span class="text-fuchsia-300">紫色</span>
-                时无法输入，需要你动动小手移动鼠标点击聚焦即可。
-              </p>
-              <p class="py-1">输入状态下想要获取提示：</p>
-              <p class="p-1">
-                1. 可以点击页面下方的按钮
-                <button class="instruction-btn">⌃ Ctrl+'</button>
-                <span class="text-gray-500">(或快捷键 Ctrl+')</span> 播放语音。
-              </p>
-              <p class="p-1">
-                2. 也可以点击按钮
-                <button class="instruction-btn">⌃ Ctrl+;</button>
-                <span class="text-gray-500">(或快捷键 Ctrl+;)</span> 显示答案。
-              </p>
-              <p class="py-1">
-                右上角图标进入 User Info
-                设置页面，可以自定义修改你喜欢的快捷键，也可以控制语音是否自动播放，更多个人设置会持续更新...
-                <i class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-sm p-[2px]"></i>
-              </p>
+                <h2 class="text-xl font-bold py-4">帮助</h2>
+                <p class="py-1">
+                  课程页面，输入下划线不是
+                  <span class="text-fuchsia-300">紫色</span>
+                  时无法输入，需要你动动小手移动鼠标点击聚焦即可。
+                </p>
+                <p class="py-1">输入状态下想要获取提示：</p>
+                <p class="p-1">
+                  1. 可以点击页面下方的按钮
+                  <button class="instruction-btn">⌃ Ctrl+'</button>
+                  <span class="text-gray-500">(或快捷键 Ctrl+')</span> 播放语音。
+                </p>
+                <p class="p-1">
+                  2. 也可以点击按钮
+                  <button class="instruction-btn">⌃ Ctrl+;</button>
+                  <span class="text-gray-500">(或快捷键 Ctrl+;)</span> 显示答案。
+                </p>
+                <p class="py-1">
+                  右上角图标进入 User Info
+                  设置页面，可以自定义修改你喜欢的快捷键，也可以控制语音是否自动播放，更多个人设置会持续更新...
+                  <i class="animate-wink inline w-1 h-8 dark:bg-white bg-slate-900 mx-2 text-sm p-[2px]"></i>
+                </p>
+              </div>
             </div>
           </div>
           <div
