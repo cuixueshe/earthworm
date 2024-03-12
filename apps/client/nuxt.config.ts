957ced958ee7aaad7e57eaa9952c6dbaf0efc576
cuixiaorui <cui_xiaorui@126.com>
2024-03-12 14:21:55 +0800

    feat: add clarity for collecting information

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const appScripts = [];
+if (process.env.NODE_ENV === "production") {
+  addClarity()
+}
+
+// for https://clarity.microsoft.com/
+function addClarity() {
+  appScripts.push({
+    innerHTML: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.CLARITY}");`,
+  });
+}
+
 export default defineNuxtConfig({
   css: ["~/assets/css/globals.css"],
   ssr: false,
@@ -14,6 +31,7 @@ export default defineNuxtConfig({
     head: {
       title: "earthworm",
       link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
+      script: appScripts,
     },
   },
   imports: {
