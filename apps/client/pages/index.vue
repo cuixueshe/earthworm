8922466cdadb2b4e5ed9f85d2580aaf722ac3266
yaolifeng <y15829485647@163.com>
2024-03-15 12:40:23 +0800

    feat: add announcement module to homepage

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -4,6 +4,23 @@
       <Loading></Loading>
     </template>
     <template v-else>
+      <section class='w-full'>
+        <div
+          class="bg-indigo-600 px-4 py-3 text-white sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+        >
+          <p class="text-center font-medium sm:text-left">
+          Earthworm is now available! 🎉
+          <br class="sm:hidden" />
+          Start your English learning journey now!
+          </p>
+          <a
+              class="mt-4 block rounded-lg bg-white px-5 py-3 text-center text-sm font-medium text-indigo-600 transition hover:bg-white/90 focus:outline-none focus:ring active:text-indigo-500 sm:mt-0"
+              href="https://github.com/cuixueshe/earthworm"
+          >
+          Learn More
+          </a>
+        </div>
+      </section>
       <section
         class="flex md:flex-row md:justify-between justify-center flex-col py-8"
       >
