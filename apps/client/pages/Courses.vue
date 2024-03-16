b6e75c00361bbda152250719109fed6e64e5c6e5
fengstats <feng2860984180@163.com>
2024-03-16 18:23:05 +0800

    style: reserve space at the bottom of the course list

--- apps/client/pages/Courses.vue
+++ apps/client/pages/Courses.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="relative w-full flex flex-col">
     <h2 class="my-4 text-3xl text-blue-500">English Course</h2>
-    <div class="h-full overflow-x-hidden overflow-y-auto scrollbar-hide">
+    <div class="h-full scrollbar-hide">
       <div
         v-if="courses.length"
-        class="h-[79vh] flex flex-wrap p-1 pb-96 gap-8 justify-start"
+        class="h-[79vh] flex flex-wrap p-1 pb-96 overflow-x-hidden overflow-y-auto gap-8 justify-start"
       >
         <template v-for="course in courses" :key="course.id">
           <NuxtLink
