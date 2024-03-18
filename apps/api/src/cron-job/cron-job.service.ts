bf8b0366cb3d6ef456cb5a8e40659472dc3c89b6
小阿熊 <786507017@qq.com>
2024-03-18 10:07:13 +0800

    fix: set time zone for cron task

--- apps/api/src/cron-job/cron-job.service.ts
+++ apps/api/src/cron-job/cron-job.service.ts
@@ -5,10 +5,13 @@ import { RankService } from '../rank/rank.service';
 @Injectable()
 export class CronJobService {
   private static readonly EVERY_MONDAY_AT_2AM = '0 2 * * 1';
+  private static readonly TIME_ZONE = 'Asia/Shanghai';
 
   constructor(private readonly rankService: RankService) {}
 
-  @Cron(CronJobService.EVERY_MONDAY_AT_2AM)
+  @Cron(CronJobService.EVERY_MONDAY_AT_2AM, {
+    timeZone: CronJobService.TIME_ZONE,
+  })
   async resetRankList() {
     this.rankService.resetRankList();
   }
