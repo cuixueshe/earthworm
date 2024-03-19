fa70c15e99a09685d3da2b03d13c59acd64d739b
cuixiaorui <cui_xiaorui@126.com>
2024-03-19 22:32:55 +0800

    refactor: xingrong courses

---
+++ packages/xingrong-courses/src/courses.ts
@@ -0,0 +1,52 @@
+import { db } from "@earthworm/db";
+import { course as courseSchema } from "@shared/schema";
+import fs from "fs";
+import path from "path";
+const courses = fs.readdirSync(path.resolve(__dirname, "../data/courses"));
+
+(async function () {
+  await db.delete(courseSchema);
+
+  for (const [index, course] of courses.entries()) {
+    const [response] = await db.insert(courseSchema).values({
+      id: index + 1,
+      title: convertToChineseNumber(path.parse(course).name),
+    });
+
+    console.log("创建:", course);
+  }
+
+  console.log("全部创建完成");
+  process.exit(0);
+})();
+
+function convertToChineseNumber(numStr: string): string {
+  const chineseNumbers = [
+    "零",
+    "一",
+    "二",
+    "三",
+    "四",
+    "五",
+    "六",
+    "七",
+    "八",
+    "九",
+    "十",
+  ];
+  let chineseStr = "第";
+  if (parseInt(numStr) >= 10) {
+    const [tens, ones] = numStr.split("");
+    if (tens !== "1") {
+      chineseStr += chineseNumbers[parseInt(tens, 10)];
+    }
+    chineseStr += "十";
+    if (ones !== "0") {
+      chineseStr += chineseNumbers[parseInt(ones, 10)];
+    }
+  } else {
+    chineseStr += chineseNumbers[parseInt(numStr, 10)];
+  }
+  chineseStr += "课";
+  return chineseStr;
+}
