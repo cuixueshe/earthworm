0822450617c4696af2c4eab74492e9b561299107
jiushi21 <zc931325671@gmail.com>
2024-03-20 22:53:37 +0800

    fix: interface Comment rename

--- apps/client/assets/comments.ts
+++ apps/client/assets/comments.ts
@@ -7,24 +7,25 @@ interface Comment {
   chinese: string;
   icon: string;
   time: number;
-  like_count: number;
+  likeCount: number;
 }
 const comments: Comment[] = [
   {
     nickname: "cuixiaorui",
-    account: 'cui_xiaorui',
+    account: "cui_xiaorui",
     avatar: "/commentsImgs/cuixiaorui.jpg",
     link: "",
     comment:
       "Earthworm is more than just a tool; It's a journey into the heart of language learning, making every sentence and conjunction an adventure. With its addictive gameplay, it's not just an exercise, it's a daily habit that invites you to take a deeper look at the richness of the English language!",
-    chinese: " Earthworm 不仅仅是一个工具;这是一次进入语言学习核心的旅程，让每个句子和连词都成为一次冒险。凭借其令人上瘾的游戏玩法，它不仅是一种练习，而且是一种日常习惯，邀请您更深入地了解英语的丰富性！",
+    chinese:
+      " Earthworm 不仅仅是一个工具;这是一次进入语言学习核心的旅程，让每个句子和连词都成为一次冒险。凭借其令人上瘾的游戏玩法，它不仅是一种练习，而且是一种日常习惯，邀请您更深入地了解英语的丰富性！",
     icon: "/twitter.svg",
     time: 1708423574717,
-    like_count: 1342,
+    likeCount: 1342,
   },
   {
     nickname: "fengstats",
-    account: 'feng_stats',
+    account: "feng_stats",
     avatar: "/commentsImgs/fengstats.png",
     link: "",
     comment:
@@ -33,11 +34,11 @@ const comments: Comment[] = [
       "我简直被这款应用迷住了！这种渐进式的学习方式，让每个阶段都充满挑战，同时也让我有了一种强烈的成就感。我已经不能停下学习的脚步，我渴望一点点的进步，希望能更上一层楼！",
     icon: "/twitter.svg",
     time: 1708409873687,
-    like_count: 197,
+    likeCount: 197,
   },
   {
     nickname: "zclsx",
-    account: 'jiushi21',
+    account: "jiushi21",
     avatar: "/commentsImgs/zclsx.jpg",
     link: "",
     comment:
@@ -45,11 +46,11 @@ const comments: Comment[] = [
     chinese: "这个应用克服了英语学习的枯燥，我现在刷课程刷得停不下来。",
     icon: "/twitter.svg",
     time: 1706005913747,
-    like_count: 79,
+    likeCount: 79,
   },
   {
     nickname: "Nauxscript",
-    account: 'Nauxscript',
+    account: "Nauxscript",
     avatar: "/commentsImgs/Nauxscript.jpg",
     link: "",
     comment:
@@ -58,23 +59,24 @@ const comments: Comment[] = [
       "现在我每天都要浏览这个应用程序，我觉得我的英语突飞猛进，有趣又充满成就感。我希望能够加入和我一样学习后进行打卡分享，会上瘾的！",
     icon: "/twitter.svg",
     time: 1706931528936,
-    like_count: 520,
+    likeCount: 520,
   },
   {
     nickname: "Ims",
-    account: 'Immerse_code',
+    account: "Immerse_code",
     avatar: "/commentsImgs/yaolifeng0629.jpg",
     link: "",
     comment:
       "Such interesting and fulfilling English learning application, it is really overwhelming. I practice it every day now, it has become a habit. Awesome!",
-    chinese: "如此有趣又充实的英语学习应用，真是让人不知所措。我现在每天都在练习，这已经成为一种习惯。棒！",
+    chinese:
+      "如此有趣又充实的英语学习应用，真是让人不知所措。我现在每天都在练习，这已经成为一种习惯。棒！",
     icon: "/twitter.svg",
     time: 1706863861905,
-    like_count: 98,
+    likeCount: 98,
   },
   {
     nickname: "vampirewy",
-    account: 'vampirewy',
+    account: "vampirewy",
     avatar: "/commentsImgs/vampirewy.jpg",
     link: "",
     comment:
@@ -83,11 +85,11 @@ const comments: Comment[] = [
       "通过句子拆分进行学习，我感觉我刷课之后，对句子的理解更加深刻。这种感觉是潜移默化的，当我第二天打开，每次都会变得更加流畅！",
     icon: "/twitter.svg",
     time: 1704763341161,
-    like_count: 86,
+    likeCount: 86,
   },
   {
     nickname: "hildxd",
-    account: 'hildxd',
+    account: "hildxd",
     avatar: "/commentsImgs/hildxd.jpg",
     link: "",
     comment:
@@ -96,11 +98,11 @@ const comments: Comment[] = [
       "再也没有像现在这样，我对学习如此充满热情。这个应用真是把学习变得如此有趣，每一次学习都像是一种挑战，让我无法抗拒。这种感觉太棒了，我能够一直刷课程而不觉得无聊",
     icon: "/twitter.svg",
     time: 1709913419937,
-    like_count: 486,
+    likeCount: 486,
   },
   {
     nickname: "zuowendong",
-    account: 'zuowendong',
+    account: "zuowendong",
     avatar: "/commentsImgs/zuowendong.jpg",
     link: "",
     comment:
@@ -109,11 +111,11 @@ const comments: Comment[] = [
       "我真的无法抵抗这款连词造句的英语学习应用的魔力，每天闲下来的时候，我都会习惯性地打开这款应用，忍不住去挑战一下自己，这对我来说是一种全新的娱乐方式！",
     icon: "/twitter.svg",
     time: 1708567188968,
-    like_count: 232,
+    likeCount: 232,
   },
   {
     nickname: "Hazel-Lin",
-    account: 'Hazel_Lin',
+    account: "Hazel_Lin",
     avatar: "/commentsImgs/Hazel-Lin.jpg",
     link: "",
     comment:
@@ -122,7 +124,7 @@ const comments: Comment[] = [
       "真没想到学习英语可以这么有趣，这个连词造句的方式简直让我停不下来。",
     icon: "/twitter.svg",
     time: 1703189419913,
-    like_count: 78,
+    likeCount: 78,
   },
 ];
 export default comments;
