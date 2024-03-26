13fd9229d6e148c460e828b748c8d99151bce14e
lck <linck@zhcpa.cn>
2024-03-26 15:21:17 +0800

    feat: change share dialog style to fix style error

--- apps/client/components/main/Share.vue
+++ apps/client/components/main/Share.vue
@@ -5,27 +5,14 @@
         ref="dialogBoxRef"
         className="modal-box w-[27rem] flex flex-col items-center overflow-hidden"
       >
-        <div
-          :class="{ skeleton: !shareImageSrc }"
-          class="flex flex-col w-[24rem] h-[32rem]"
-          ref="imageContainer"
-        >
-          <img
-            v-show="shareImageSrc"
-            :src="shareImageSrc"
-            alt=""
-            width="400"
-            height="600"
-            class="rounded-md"
-          />
-        </div>
-        <div class="gallery flex w-full py-2">
+      <div class="flex">
+        <div class="gallery py-2 mr-2">
           <div
             :class="{
               '!border-primary': currImageIndex === index,
               skeleton: !imgItem.src,
             }"
-            class="border-2 border-transparent gallery-item w-12 h-18 cursor-pointer rounded-sm overflow-hidden mr-2"
+            class="border-2 border-transparent gallery-item w-14 mb-2 h-18 cursor-pointer rounded-sm overflow-hidden mr-2"
             @click="handleSelectImage(index)"
             v-for="(imgItem, index) in galleryImgs"
             :key="imgItem.src"
@@ -37,6 +24,21 @@
             />
           </div>
         </div>
+        <div
+          :class="{ skeleton: !shareImageSrc }"
+          class="w-[19rem] h-[27rem]"
+          ref="imageContainer"
+        >
+          <img
+            v-show="shareImageSrc"
+            :src="shareImageSrc"
+            alt=""
+            width="400"
+            height="600"
+            class="rounded-md"
+          />
+        </div>
+      </div>
         <div className="modal-action">
           <button class="btn btn-primary" @click="copyAndClose">
             复制并关闭
@@ -52,8 +54,8 @@
 import { ref, watch } from "vue";
 import { convertTitleToNumber } from "~/composables/main/shareImage/convert";
 import {
-  useGenerateShareImage,
-  useShareModal,
+useGenerateShareImage,
+useShareModal,
 } from "~/composables/main/shareImage/share";
 import { useCourseStore } from "~/store/course";
 import { useUserStore } from "~/store/user";
@@ -72,6 +74,7 @@ const {
   handleSelectImage,
   currImageIndex,
 } = useGenerateShareImage();
+
 watch(shareModalVisible, (newVal) => {
   if (newVal && courseStore.currentCourse?.title) {
     console.log(userStore.user);
