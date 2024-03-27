82c8848688e869c13fa7ccda64eb5b2fad8d9ebc
sungang <15893531505@163.com>
2024-03-27 10:47:43 +0800

    feat: unify setting page style（#395）

--- apps/client/components/user/Setting.vue
+++ apps/client/components/user/Setting.vue
@@ -1,108 +1,119 @@
 <template>
-  <div class="space-y-4">
-    <section>
-      <h2 class="text-lg mb-2">快捷键设置</h2>
-      <n-table
-        :bordered="false"
-        :single-line="false"
-      >
+  <div class="space-y-4 min-w-max">
+    <section class="space-y-2">
+      <h2 class="text-lg">快捷键设置</h2>
+      <table class="table">
         <thead>
           <tr>
-            <th>命令</th>
-            <th>键绑定</th>
-            <th>操作</th>
+            <th class="text-base">命令</th>
+            <th class="text-base">键绑定</th>
+            <th class="w-[300px] text-base text-center">操作</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>播放发音</td>
+          <tr class="hover">
+            <td class="label-text">播放发音</td>
             <td>{{ shortcutKeys.sound }}</td>
-            <td>
-              <n-button
-                text
+            <td class="text-center">
+              <button
+                class="btn btn-sm btn-outline btn-secondary"
                 @click="handleEdit('sound')"
               >
                 编辑
-              </n-button>
+              </button>
             </td>
           </tr>
-          <tr>
-            <td>切换答题/答案页面</td>
+          <tr class="hover">
+            <td class="label-text">切换答题/答案页面</td>
             <td>{{ shortcutKeys.answer }}</td>
-            <td>
-              <n-button
-                text
+            <td class="text-center">
+              <button
+                class="btn btn-sm btn-outline btn-secondary"
                 @click="handleEdit('answer')"
               >
                 编辑
-              </n-button>
+              </button>
             </td>
           </tr>
         </tbody>
-      </n-table>
+      </table>
     </section>
 
-    <section>
-      <h2 class="text-lg mb-2">声音设置</h2>
-      <div class="form-control w-80">
-        <label class="cursor-pointer label">
-          <span class="label-text">开启键盘打字音效</span>
-          <input
-            type="checkbox"
-            class="toggle toggle-primary"
-            :checked="keyboardSound"
-            @change="toggleKeyboardSound"
-          />
-        </label>
-      </div>
-      <div class="form-control w-80">
-        <label class="cursor-pointer label">
-          <span class="label-text">自动播放声音（答案页面）</span>
-          <input
-            type="checkbox"
-            class="toggle toggle-primary"
-            :checked="autoPlaySound"
-            @change="toggleAutoPlaySound"
-          />
-        </label>
-      </div>
-      <div class="form-control w-80">
-        <label class="cursor-pointer label">
-          <span class="label-text">切换口音</span>
-          <n-select
-            v-model:value="pronunciation"
-            :options="getPronunciationOptions()"
-            :on-update-value="togglePronunciation"
-            class="w-[90px]"
-          />
-        </label>
-      </div>
+    <section class="space-y-2">
+      <h2 class="text-lg">声音设置</h2>
+      <table class="table">
+        <tbody>
+          <tr class="hover">
+            <td class="label-text">开启键盘打字音效</td>
+            <td class="w-[300px] text-center">
+              <input
+                type="checkbox"
+                class="toggle toggle-secondary"
+                :checked="keyboardSound"
+                @change="toggleKeyboardSound"
+              />
+            </td>
+          </tr>
+          <tr class="hover">
+            <td class="label-text">自动播放声音（答案页面）</td>
+            <td class="w-[300px] text-center">
+              <input
+                type="checkbox"
+                class="toggle toggle-secondary"
+                :checked="autoPlaySound"
+                @change="toggleAutoPlaySound"
+              />
+            </td>
+          </tr>
+          <tr class="hover">
+            <td class="label-text">切换口音</td>
+            <td class="w-[300px] text-center">
+              <div class="join">
+                <input
+                  v-for="lang in getPronunciationOptions()"
+                  class="join-item btn btn-sm"
+                  type="radio"
+                  name="options"
+                  :value="lang.value"
+                  :aria-label="lang.label"
+                  :checked="pronunciation === lang.value"
+                  @change="togglePronunciation(lang.value as PronunciationType)"
+                />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </section>
 
-    <section>
-      <h2 class="text-lg mb-2">答题设置</h2>
-      <div class="form-control w-80">
-        <label class="cursor-pointer label">
-          <span class="label-text">显示每个单词长度</span>
-          <input
-            type="checkbox"
-            class="toggle toggle-primary"
-            :checked="showWordsWidth"
-            @change="toggleAutoWordsWidth"
-          />
-        </label>
-      </div>
-      <div class="form-control w-80">
-        <label class="cursor-pointer label">
-          <span class="label-text">开启空格提交答案</span>
-          <input
-            type="checkbox"
-            class="toggle toggle-primary"
-            :checked="useSpace"
-            @change="toggleUseSpaceSubmitAnswer"
-          />
-        </label>
-      </div>
+    <section class="space-y-2">
+      <h2 class="text-lg">答题设置</h2>
+      <table class="table">
+        <tbody>
+          <tr class="hover">
+            <td class="label-text">显示每个单词长度</td>
+            <td class="w-[300px] text-center">
+              <input
+                type="checkbox"
+                class="toggle toggle-secondary"
+                :checked="showWordsWidth"
+                @change="toggleAutoWordsWidth"
+              />
+            </td>
+          </tr>
+          <tr class="hover">
+            <td class="label-text">开启空格提交答案</td>
+            <td class="w-[300px] text-center">
+              <input
+                type="checkbox"
+                class="toggle toggle-secondary"
+                :checked="useSpace"
+                @change="toggleUseSpaceSubmitAnswer"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   </div>
 
@@ -117,9 +128,7 @@
       <h3 class="mb-4 text-center text-base font-bold text-fuchsia-500">
         请先按下单键/组合键，通过回车键（Enter ⏎）来设置
       </h3>
-      <div
-        class="h-8 leading-8 border border-solid border-fuchsia-500 rounded text-center"
-      >
+      <div class="h-8 leading-8 border border-solid border-fuchsia-500 rounded text-center">
         {{ shortcutKeyStr }}
       </div>
       <div class="text-center mt-2 text-xs">
@@ -131,7 +140,7 @@
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
-import { usePronunciation } from "~/composables/user/pronunciation";
+import { PronunciationType, usePronunciation } from "~/composables/user/pronunciation";
 import { useShortcutKeyMode } from "~/composables/user/shortcutKey";
 import {
   useAutoPronunciation,
@@ -177,3 +186,15 @@ onUnmounted(() => {
   document.removeEventListener("keydown", handleKeydown);
 });
 </script>
+
+<style scoped>
+.btn-outline.btn-secondary:hover,
+.toggle-secondary:checked,
+.btn:is(input[type="radio"]:checked) {
+  @apply text-[#ffffff] border-fuchsia-500 bg-fuchsia-500;
+}
+
+.btn-outline.btn-secondary {
+  @apply text-fuchsia-500 outline-fuchsia-500;
+}
+</style>
