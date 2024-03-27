a8727b321030e712fe9942e657e6798e39f90e03
jiushi21 <zc931325671@gmail.com>
2024-03-27 19:52:06 +0800

    fix: delete more svg

--- apps/client/pages/Index/components/Questions.vue
+++ apps/client/pages/Index/components/Questions.vue
@@ -25,35 +25,6 @@
             <span class="icon">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 size-5 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 size-5 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
                 class="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
