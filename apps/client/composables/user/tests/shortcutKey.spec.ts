e8890c5e5535f5f983d719546e3b38ceafd33d73
cuixiaorui <cui_xiaorui@126.com>
2024-03-29 20:36:10 +0800

    test: fix test case

--- apps/client/composables/user/tests/shortcutKey.spec.ts
+++ apps/client/composables/user/tests/shortcutKey.spec.ts
@@ -25,7 +25,8 @@ describe("user defined shortcut key", () => {
     it("should be equal to cache data if localStorage has cache", () => {
       const storeShortcutKeys = {
         sound: "Ctrl+s",
-        answer: "Ctrl+.",
+        answer: "Ctrl+8",
+        skip: "Ctrl+.",
       };
 
       localStorage.setItem(SHORTCUT_KEYS, JSON.stringify(storeShortcutKeys));
