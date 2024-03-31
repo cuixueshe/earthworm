bba8a9eea1d9c42700e1e0ce9fea51c3500f6203
fengstats <feng2860984180@163.com>
2024-03-31 21:57:52 +0800

    fix: zoom out AnswerTip width

--- apps/client/components/main/AnswerTip.vue
+++ apps/client/components/main/AnswerTip.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute left-0 flex items-center justify-center w-full text-xl top-36 dark:text-gray-50"
+    class="absolute flex items-center justify-center w-3/4 text-xl top-36 dark:text-gray-50"
   >
     <div class="shadow-xl card bg-base-100">
       <div class="relative card-body">
