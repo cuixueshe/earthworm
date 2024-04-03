e5825de5746408b9a108ce668b57cac53503c0e8
linhuizi <529246402@qq.com>
2024-04-03 18:01:53 +0800

    fix: update achievement card

--- apps/client/components/user/AchievementCard.vue
+++ apps/client/components/user/AchievementCard.vue
@@ -1,23 +1,71 @@
 <template>
-  <div class="card achievement">
-    <div class="active">
+  <div class="cursor-pointer card achievement">
+    <div
+      class="check-box"
+      v-if="isShowCheckBox"
+    >
+      <input
+        type="checkbox"
+        :checked="isChecked"
+        class="rounded checkbox checkbox-xs [--chkbg:fuchsia]"
+        @change="toggleChooseAchievement"
+        v-model="isChecked"
+      />
+    </div>
+    <div
+      class="active"
+      v-if="isActive"
+    >
       <span class="active-label">使用中</span>
     </div>
-    <figure>
-      <img
-        src="/contributor.png"
-        alt="contributor"
-        class="rounded-xl w-[110px] h-[110px] mb-5"
-      />
-    </figure>
-    <div class="items-center justify-center text-center">
-      <p class="mb-3 text-gray-500 active-name">成就名</p>
-      <p class="text-xs text-gray-500 active-name">获得时间: 2024/4/2</p>
+    <div
+      class="tooltip"
+      :data-tip="desc"
+    >
+      <figure>
+        <img
+          :src="avatar"
+          alt="contributor"
+          class="rounded-xl w-[110px] h-[110px] mb-5"
+        />
+      </figure>
+      <div class="items-center justify-center text-center">
+        <p class="mb-3 text-gray-500 active-name">{{ name }}</p>
+        <p
+          class="text-xs text-gray-500 active-name"
+          v-if="createdAt"
+        >
+          获得时间: {{ createdAt }}
+        </p>
+        <!-- <p class="text-xs text-gray-500 active-name">暂未获得</p> -->
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import type { AchievementItem } from "~/composables/user/achievement";
+
+const props = withDefaults(
+  defineProps<{
+    achievement: AchievementItem;
+    isShowCheckBox?: boolean;
+  }>(),
+  { isShowCheckBox: false }
+);
+const emit = defineEmits(["toggleChooseAchievement"]);
+function toggleChooseAchievement() {
+  emit("toggleChooseAchievement", props.achievement);
+}
+const isShowCheckBox = computed(() => props.isShowCheckBox);
+const isActive = computed(() => props.achievement?.isActive);
+const name = computed(() => props.achievement.name);
+const createdAt = computed(() => props.achievement.createdAt);
+const avatar = computed(() => props.achievement.avatar);
+const desc = computed(() => props.achievement.desc);
+const isChecked = computed(() => props.achievement.isChecked);
+</script>
 <style scoped>
 .achievement {
   position: relative;
@@ -28,8 +76,7 @@
   border: 1px solid rgba(166, 166, 166, 1);
   box-shadow: 0px 8px 24px rgba(149, 157, 165, 0.2);
   border: 1px solid rgba(232, 121, 249, 1);
-
-  @apply rounded-md;
+  @apply rounded-md hover:shadow-lg;
 }
 .active {
   position: absolute;
@@ -44,6 +91,15 @@
   justify-content: center;
   align-items: center;
   padding: 2px 6px 2px 6px;
+}
+.check-box {
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .active-label {
   font-size: 8px;
--- apps/client/composables/user/achievement.ts
+++ apps/client/composables/user/achievement.ts
@@ -1,8 +1,41 @@
-interface AchievementItem {
+import { ref } from "vue";
+export interface AchievementItem {
   id: number;
   name: string;
   desc: string;
   avatar: string;
-  achieved: boolean;
-  createdAt: string;
+  achieved?: boolean;
+  createdAt?: string;
+  isActive?: boolean;
+  isChecked?: boolean;
 }
+
+/** 成就模块处理逻辑
+ *  achievementList: 当前用户成就列表
+ *  getAchievementList: 获取当前用户的所有的成就列表
+ *  setAchievementActive: 设置成就为使用中状态
+ */
+
+export const useAchievementList = () => {
+  const achievementList = ref<AchievementItem[]>([]);
+  const getAchievementList = async () => {
+    // const res = await UserAchievement.getAchievementList();
+    // achievementList.value = res.data;
+  };
+  const setAchievementActive = async (id: number) => {
+    // const res = await UserAchievement.setAchievementActive(id);
+    // if (res.code === 200) {
+    //   achievementList.value = achievementList.value.map((item) => {
+    //     if (item.id === id) {
+    //       item.isActive = true;
+    //     }
+    //     return item;
+    //   });
+    // }
+  };
+  return {
+    achievementList,
+    getAchievementList,
+    setAchievementActive,
+  };
+};
