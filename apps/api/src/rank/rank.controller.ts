b55b272ba94b207394c893e019c662b3e5d8ed10
MasterJyh <52491970+masterjiyuhang@users.noreply.github.com>
2024-04-08 11:27:43 +0800

    feat: add swagger api docs (#483)
    * feat: add swagger api docs
    
    * fix: remove redundant api info

--- apps/api/src/rank/rank.controller.ts
+++ apps/api/src/rank/rank.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthGuard, UncheckAuth } from '../auth/auth.guard';
 import { User, UserEntity } from '../user/user.decorators';
 import type { RankPeriodAlias } from './rank.service';
 import { RankPeriod, RankService } from './rank.service';
 
+@ApiBearerAuth()
+@ApiTags('Rank')
 @Controller('rank')
 export class RankController {
   constructor(private readonly rankService: RankService) {}
 
+  @ApiOperation({
+    summary: '获取排行榜信息',
+  })
   @UncheckAuth()
   @UseGuards(AuthGuard)
   @Get('progress/:period')
