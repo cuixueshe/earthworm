06d05a1b7d3670c24b57ff2a3f80c9336355db26
liao <im5@qq.com>
2024-04-10 22:57:12 +0800

    fix(loading): Correct loading display on homepage redirection

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -66,8 +66,8 @@ function useShortcutToGame() {
   async function startEarthworm() {
     isLoading.value = true;
     const { courseId } = await gameStore.startGame();
+    await router.push(`/main/${courseId}`);
     isLoading.value = false;
-    router.push(`/main/${courseId}`);
   }
 
   onMounted(() => {
