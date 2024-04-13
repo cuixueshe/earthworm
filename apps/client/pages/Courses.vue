fffbc4519d5e4bc35b49bc35d74b7fbd5442e304
fengstats <feng2860984180@163.com>
2024-04-13 21:16:53 +0800

    fix: adjust course list right margin

--- apps/client/pages/Courses.vue
+++ apps/client/pages/Courses.vue
@@ -4,7 +4,7 @@
     <div class="h-full scrollbar-hide">
       <div
         v-if="courses.length"
-        class="h-[79vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-1 pb-96 overflow-x-hidden overflow-y-auto gap-8 justify-start"
+        class="h-[79vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-1 pr-4 pb-96 overflow-x-hidden overflow-y-auto gap-8 justify-start"
       >
         <template
           v-for="course in courses"
