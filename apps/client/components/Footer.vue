03279cf6f76546174a298923b583d70103b7be24
cuixiaorui <cui_xiaorui@126.com>
2024-04-14 12:22:36 +0800

    feat: add Internet Content Provider

--- apps/client/components/Footer.vue
+++ apps/client/components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <footer class="h-12 flex justify-center items-center opacity-65 shrink-0 text-xs">
-    Copyright © 2023-2024, Earthworm. All rights reserved.
+  <footer
+    class="h-12 flex flex-col justify-center items-center opacity-65 shrink-0 text-xs"
+  >
+    <p>Copyright © 2023-2024, Earthworm. All rights reserved.</p>
+    <p class="text-xs">
+      <a href="https://beian.miit.gov.cn/">京ICP备2020042122号-2</a>
+    </p>
+    t
   </footer>
 </template>
