37c022aba1dac296fc726d672ab8640975ecf175
cuixiaorui <cui_xiaorui@126.com>
2024-04-14 13:13:57 +0800

    chore: Update Footer.vue to remove commented out code

--- apps/client/components/Footer.vue
+++ apps/client/components/Footer.vue
@@ -3,9 +3,10 @@
     class="h-12 flex flex-col justify-center items-center opacity-65 shrink-0 text-xs"
   >
     <p>Copyright © 2023-2024, Earthworm. All rights reserved.</p>
-    <p class="text-xs">
+    <!-- <p class="text-xs">
+      // TODO 当服务器迁移到国内时在做处理
       <a href="https://beian.miit.gov.cn/">京ICP备2020042122号-2</a>
-    </p>
+    </p> -->
     t
   </footer>
 </template>
