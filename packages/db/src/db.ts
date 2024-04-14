da8f9536bf8f6c5410388f839e44c9003847a3b4
hildxd <hildxd@outlook.com>
2024-04-14 11:14:21 +0800

    chore: mysql replaced with postgres

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,20 +1,12 @@
-import dotenv from 'dotenv';
-import { drizzle } from 'drizzle-orm/mysql2';
-import * as mysql from 'mysql2/promise';
-import path from 'node:path';
+import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import path from "node:path";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../apps/api/.env") });
 
-const connection = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  multipleStatements: true,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-});
+console.log("connection string: ", process.env.DATABASE_URL);
+const connection = postgres(process.env.DATABASE_URL ?? "");
 
 export const db = drizzle(connection);
