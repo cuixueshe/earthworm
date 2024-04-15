229c16ac51d88d8bf8400a37483f76cc1acba08c
ryanzhouFQJ <ryanzhouFQJ>
2024-04-15 15:21:07 +0800

    fix: 解决合并冲突

--- apps/client/components/Navbar.vue
+++ apps/client/components/Navbar.vue
@@ -1,14 +1,13 @@
 <template>
-<<<<<<< Updated upstream
   <header
     :class="isStickyNavBar"
-    class="w-full top-0 bg-opacity-50 backdrop-blur-xl font-customFont z-40"
+    class="top-0 z-40 w-full bg-opacity-50 backdrop-blur-xl font-customFont"
   >
-    <div class="mx-auto max-w-screen-xl px-6">
-      <div class="flex h-16 items-center justify-between">
-        <div class="flex flex-1 items-center justify-between">
+    <div class="max-w-screen-xl px-6 mx-auto">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center justify-between flex-1">
           <NuxtLink to="/">
-            <div class="logo flex items-center">
+            <div class="flex items-center logo">
               <img
                 width="48"
                 height="48"
@@ -51,7 +50,7 @@
           <!-- 显示用户信息 -->
           <div
             v-if="isAuthenticated()"
-            class="logged-in flex items-center"
+            class="flex items-center logged-in"
           >
             <div
               class="mx-2 font-500 truncate min-[500px]:max-w-[6em] max-w-[4em]"
@@ -66,14 +65,14 @@
             v-else
             @click="signIn()"
             aria-label="Login"
-            class="btn btn-sm btn-ghost text-base font-normal dark:text-white rounded-md mx-1 h-8 px-4"
+            class="h-8 px-4 mx-1 text-base font-normal rounded-md btn btn-sm btn-ghost dark:text-white"
           >
             <span class="relative">登录</span>
           </button>
 
           <!-- 切换主题 -->
           <button
-            class="btn btn-sm btn-ghost rounded-md mx-1 w-8 h-8 p-0"
+            class="w-8 h-8 p-0 mx-1 rounded-md btn btn-sm btn-ghost"
             @click="toggleDarkMode"
           >
             <svg
@@ -112,135 +111,20 @@
     v-model:isShowModal="isShowModal"
     title="提示"
     content="是否确认退出登录？"
-    @confirm="signOut()"
+    @confirm="handleSignOut()"
   />
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
-=======
-  <nav class="flex items-center justify-between h-20 shrink-0">
-    <NuxtLink to="/">
-      <div class="flex items-center logo">
-        <img
-          width="48"
-          height="48"
-          class="mr-6 overflow-hidden rounded-md"
-          src="/logo.png"
-          alt="earth-worm-logo"
-        />
-        <h1
-          class="text-2xl font-black leading-none leading-normal text-wrap text-fuchsia-400"
-        >
-          Earthworm
-        </h1>
-      </div>
-    </NuxtLink>
-    <div class="flex items-center">
-      <button
-        v-if="!userStore.user && route.name !== 'Auth-Login'"
-        class="h-8 px-2 mx-1 btn btn-sm btn-ghost"
-        @click="handleLogin"
-      >
-        Log in
-      </button>
-      <button
-        v-else-if="!userStore.user && route.name !== 'Auth-Signup'"
-        class="h-8 px-2 mx-1 btn btn-sm btn-ghost"
-        @click="handleSignup"
-      >
-        Sign up
-      </button>
-      <div v-else class="dropdown dropdown-end">
-        <button
-          tabindex="0"
-          class="w-8 h-8 p-0 mx-1 rounded-md btn btn-sm btn-ghost"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-            />
-          </svg>
-        </button>
-        <ul
-          tabindex="0"
-          class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
-        >
-          <li @click="handleViewUserInfo"><a>User info</a></li>
-          <li @click="handleLogout"><a>Log out</a></li>
-          <!-- <li><a>Item 2</a></li> -->
-        </ul>
-      </div>
-      <button
-        class="w-8 h-8 p-0 mx-1 rounded-md btn btn-sm btn-ghost"
-        @click="toggleDarkMode"
-      >
-        <svg
-          v-if="isDarkMode"
-          xmlns="http://www.w3.org/2000/svg"
-          width="1.5em"
-          height="1.5em"
-          viewBox="0 0 256 256"
-        >
-          <path
-            fill="currentColor"
-            d="M233.54 142.23a8 8 0 0 0-8-2a88.08 88.08 0 0 1-109.8-109.8a8 8 0 0 0-10-10a104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.09 103.09 0 0 0 62.52-20.88a104.84 104.84 0 0 0 37-52.91a8 8 0 0 0-1.98-7.98m-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104a106 106 0 0 0 14.92-1.06a89 89 0 0 1-26.02 31.4"
-          />
-        </svg>
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-          />
-        </svg>
-      </button>
-    </div>
-    <MessageBox
-      v-model:isShowModal="isShowModal"
-      title="Notice"
-      content="Are you sure to exit?"
-      @confirm="handleLogoutConfirm"
-    />
-  </nav>
-</template>
-
-<script setup lang="ts">
-import { ref } from "vue";
-import { useMessage } from "naive-ui";
-import { navigateTo } from "nuxt/app";
->>>>>>> Stashed changes
 import { useRoute } from "vue-router";
 import { Theme, useDarkMode } from "~/composables/darkMode";
 import { isAuthenticated, signIn, signOut } from "~/services/auth";
-import { useUserStore } from "~/store/user";
-<<<<<<< Updated upstream
-=======
-import { cleanToken } from "~/utils/token";
-import { computed } from "vue";
 import { useCalendarStore } from "~/store/calendar";
->>>>>>> Stashed changes
+import { useUserStore } from "~/store/user";
 
 const route = useRoute();
 const userStore = useUserStore();
-<<<<<<< Updated upstream
 const { darkMode, toggleDarkMode } = useDarkMode();
 
 const isShowModal = ref(false);
@@ -259,48 +143,13 @@ const isStickyNavBar = computed(() => {
   }
   return "";
 });
-=======
-const calendarStore = useCalendarStore();
-
-const isShowModal = ref(false);
-
-const { setDarkMode, toggleDarkMode, darkMode } = useDarkMode();
-
-const isDarkMode = computed(() => darkMode.value === Theme.DARK);
-
-const handleViewUserInfo = () => {
-  navigateTo("/user/info");
-};
-
-const handleLogin = () => {
-  navigateTo("/auth/login");
-};
-
-const handleSignup = () => {
-  navigateTo("/auth/signup");
-};
-
-const message = useMessage();
->>>>>>> Stashed changes
 
 const handleLogout = () => {
   isShowModal.value = true;
 };
-<<<<<<< Updated upstream
-</script>
-=======
-
-const handleLogoutConfirm = () => {
-  userStore.logoutUser();
+const handleSignOut = () => {
+  signOut();
+  const calendarStore = useCalendarStore();
   calendarStore.logoutCalendar();
-  cleanToken();
-  message.success("logout success", {
-    duration: 500,
-    onLeave() {
-      navigateTo("/");
-    },
-  });
 };
 </script>
-<style></style>
->>>>>>> Stashed changes
--- apps/client/components/main/Answer.vue
+++ apps/client/components/main/Answer.vue
@@ -3,11 +3,7 @@
     <div class="ml-8 text-5xl text-fuchsia-500 dark:text-gray-50">
       {{ courseStore.currentStatement?.english }}
       <svg
-<<<<<<< Updated upstream
         class="inline-block ml-1 cursor-pointer w-7 h-7 fill-gray-500 hover:fill-fuchsia-500"
-=======
-        class="inline-block ml-1 cursor-pointer w-7 h-7"
->>>>>>> Stashed changes
         viewBox="0 0 1024 1024"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
@@ -15,10 +11,6 @@
       >
         <path
           d="M342.4 384H128v256h214.4L576 826.8V197.2L342.4 384zM64 320h256L640 64v896L320 704H64V320z m640 256h256v-64H704v64z m16.8 159.5l181 181 45.3-45.3-181-181-45.3 45.3z m33.9-343.9l181-181-45.3-45.3-181 181 45.3 45.3z"
-<<<<<<< Updated upstream
-=======
-          fill="#666666"
->>>>>>> Stashed changes
         ></path>
       </svg>
     </div>
@@ -48,26 +40,18 @@ import { onMounted, onUnmounted } from "vue";
 import { useCurrentStatementEnglishSound } from "~/composables/main/englishSound";
 import { useGameMode } from "~/composables/main/game";
 import { useSummary } from "~/composables/main/summary";
-<<<<<<< Updated upstream
 import { useAutoPronunciation } from "~/composables/user/sound";
+import { useCalendarStore } from "~/store/calendar";
 import { useCourseStore } from "~/store/course";
 import { cancelShortcut, registerShortcut } from "~/utils/keyboardShortcuts";
-=======
-import { useAutoSound } from "~/composables/user/sound";
-import { useCalendarStore } from "~/store/calendar";
->>>>>>> Stashed changes
 
 const courseStore = useCourseStore();
 const { handlePlaySound } = usePlayEnglishSound();
 const { showSummary } = useSummary();
 const { showQuestion } = useGameMode();
-<<<<<<< Updated upstream
 const { isAutoPlaySound } = useAutoPronunciation();
 
 registerShortcutKeyForNextQuestion();
-=======
-const { isAutoPlaySound } = useAutoSound();
->>>>>>> Stashed changes
 
 function usePlayEnglishSound() {
   const { playSound } = useCurrentStatementEnglishSound();
