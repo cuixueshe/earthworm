eaa63b44a8e1cfb5d09ef7bfaffca509d35186f6
cuixiaorui <cui_xiaorui@126.com>
2024-04-28 16:19:21 +0800

    chore: update drizzle

--- packages/db/drizzle/0000_closed_susan_delgado.sql
+++ packages/db/drizzle/0000_closed_susan_delgado.sql
@@ -1,28 +1,29 @@
 CREATE TABLE IF NOT EXISTS "courses" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
 	"title" varchar(256) NOT NULL,
-	"course_pack_id" integer NOT NULL
+	"order" integer NOT NULL,
+	"course_pack_id" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "course_history" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
-	"course_id" integer NOT NULL,
-	"course_pack_id" integer NOT NULL,
-	"completion_count" integer DEFAULT 0 NOT NULL,
+	"course_id" text NOT NULL,
+	"course_pack_id" text NOT NULL,
+	"completion_count" integer NOT NULL,
 	CONSTRAINT "course_history_user_id_course_id_course_pack_id_unique" UNIQUE("user_id","course_id","course_pack_id")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "course_packs" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
+	"order" integer NOT NULL,
 	"title" text NOT NULL,
 	"description" text,
-	"progress" integer,
 	"is_free" boolean
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "memberships" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
 	"start_date" timestamp NOT NULL,
 	"end_date" timestamp NOT NULL,
@@ -30,29 +31,31 @@ CREATE TABLE IF NOT EXISTS "memberships" (
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "statements" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
 	"order" integer NOT NULL,
 	"chinese" text NOT NULL,
 	"english" text NOT NULL,
 	"soundmark" text NOT NULL,
-	"course_id" integer NOT NULL,
-	CONSTRAINT "statements_order_unique" UNIQUE("order")
+	"course_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user_course_progress" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"course_pack_id" text NOT NULL,
+	"course_id" text NOT NULL,
+	"statement_index" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp,
+	CONSTRAINT "user_course_progress_user_id_course_id_course_pack_id_unique" UNIQUE("user_id","course_id","course_pack_id")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_learn_record" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
 	"count" integer DEFAULT 0 NOT NULL,
 	"day" date NOT NULL,
 	CONSTRAINT "user_learn_record_user_id_day_unique" UNIQUE("user_id","day")
-);
---> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "user_course_progress" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"course_id" integer NOT NULL,
-	"user_id" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp
 );
 --> statement-breakpoint
 DO $$ BEGIN
--- packages/db/drizzle/0001_true_ben_urich.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "user_course_progress" ADD COLUMN "course_pack_id" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_course_progress" ADD COLUMN "statement_index" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "course_packs" DROP COLUMN IF EXISTS "progress";--> statement-breakpoint
-ALTER TABLE "user_course_progress" ADD CONSTRAINT "user_course_progress_user_id_course_id_course_pack_id_unique" UNIQUE("user_id","course_id","course_pack_id");
