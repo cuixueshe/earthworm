92fe80484011d21ab03e59a0817c46a60b9ce0c8
fengstats <feng2860984180@163.com>
2024-05-01 17:40:08 +0800

    style: replace svg to icon

--- apps/client/components/Landing/index.vue
+++ apps/client/components/Landing/index.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="container m-auto w-full font-customFont">
-    <!-- 提醒老用户需要重新去注册 
-    1周后删除 -->
+    <!-- 提醒老用户需要重新去注册 1 周后删除 -->
     <div
       role="alert"
-      class="alert alert-warning"
+      class="alert alert-warning mt-4"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 shrink-0 stroke-current"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-        />
-      </svg>
+      <span class="i-ph-warning-circle h-6 w-6"></span>
       <p>
         亲爱的用户，我们升级了系统以支持多课程和邮箱注册。原有版本为 MVP
         版本，已经删档。请重新注册，体验新特性。为此带来不便，我们深表歉意，感谢您的理解 💗
