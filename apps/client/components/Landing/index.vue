8836768e1a977a5a3b9770212d235e22908f7aff
fengstats <feng2860984180@163.com>
2024-05-02 12:30:02 +0800

    style: optimize the top notification box

--- apps/client/components/Landing/index.vue
+++ apps/client/components/Landing/index.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="container m-auto w-full font-customFont">
+  <div class="font-customFont">
     <!-- 提醒老用户需要重新去注册 1 周后删除 -->
     <div
       role="alert"
-      class="alert alert-warning mt-4"
+      class="alert alert-warning mt-4 flex flex-col items-center md:flex-row md:items-start"
     >
-      <span class="i-ph-warning-circle h-6 w-6"></span>
-      <p>
-        亲爱的用户，我们升级了系统以支持多课程和邮箱注册。原有版本为 MVP
-        版本，已经删档。请重新注册，体验新特性。为此带来不便，我们深表歉意，感谢您的理解 💗
+      <p class="leading-7">
+        <span class="i-ph-warning-circle relative top-1 h-5 w-5"></span>
+        亲爱的用户，我们升级了系统以支持多课程和邮箱注册。原有版本为 MVP 版本，已经删档。
+        请重新注册，体验更多新特性！为此带来不便，我们深表歉意，感谢您的理解
+        <a
+          class="link link-secondary underline-offset-4"
+          @click="signIn()"
+        >
+          去注册
+        </a>
+        💗
       </p>
-      <button
-        className="btn btn-sm text-purple-400"
-        @click="signIn()"
-      >
-        去注册
-      </button>
     </div>
     <LandingBanner @start-earthworm="startEarthworm" />
     <LandingFeatures />
