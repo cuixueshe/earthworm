6b93dcbd047c854a61bd918c2f8b5ba9d0f407a5
小小石 <48991003+fengstats@users.noreply.github.com>
2024-05-16 03:59:01 +0800

    style: optimize home page layout (#687)

--- apps/client/components/Home/index.vue
+++ apps/client/components/Home/index.vue
@@ -1,71 +1,51 @@
 <template>
-  <div class="flex h-full w-full">
-    <div class="mr-10 hidden max-w-[230px] md:block">
-      <div class="md-4 flex flex-col md:mr-10">
-        <div
-          class="h-[230px] w-[230px] overflow-hidden rounded-full border border-gray-200 p-[2px] dark:border-gray-600"
-        >
-          <img
-            :src="userStore.userInfo?.picture!"
-            class="h-full w-full cursor-pointer"
-          />
-        </div>
-        <div class="mt-6 text-2xl font-bold">{{ userStore.userInfo?.username }}</div>
-        <div class="text-xl font-light text-gray-400">
-          {{ userStore.userInfo?.name }}
-        </div>
-        <!-- TODO: 等勋章功能上线后，再显示此区域-->
-        <!-- <div
-          class="border-t-solid border-t-black-200 mt-5 flex flex-col border-t border-gray-300 pt-5 dark:border-gray-700"
-        >
-          <div class="text-md">勋章</div>
-          <div class="mt-3 grid grid-cols-3 gap-2">
-            <div
-              class="m-auto flex-shrink-0 overflow-hidden rounded-full"
-              v-for="i in 12"
-              :key="i"
-            >
-              <img
-                src="https://sdfsdf.dev/72x72.png"
-                alt=""
-                class="flex-shrink-0"
-              />
-            </div>
-          </div>
-        </div> -->
-      </div>
-    </div>
-    <div
-      class="flex flex-1 flex-col justify-between rounded-md border border-gray-300 p-5 dark:border-gray-700"
-    >
-      <div class="top">
-        <div class="flex items-center">
-          <div class="mr-auto text-2xl font-bold">最近使用的课程包</div>
-          <NuxtLink
-            href="/course-pack"
-            class="btn btn-primary btn-sm mr-2 border-none bg-[#1f6feb] text-white hover:bg-[#1f6feb]"
-            >更多课程包</NuxtLink
-          >
-          <!-- <NuxtLink
-          class="btn btn-primary btn-sm"
-          href="/course-pack"
-          >更多音乐</NuxtLink
-        > -->
-        </div>
-        <HomeRecentCoursePack class="mt-3 md:mt-5" />
-      </div>
-      <div class="w-full rounded-xl">
-        <hr class="my-3 border-gray-300 dark:border-gray-700 md:my-5" />
-        <HomeCalendarGraph
-          :data="learnRecord.list"
-          :totalCount="learnRecord.totalCount"
-          @toggleYear="toggleYear"
+  <div class="mt-8 flex w-full justify-between">
+    <!-- 左侧头像区域 -->
+    <div class="mr-16 hidden w-72 md:block">
+      <div class="mx-auto h-56 w-56 overflow-hidden">
+        <img
+          class="h-full w-full rounded-full border-2 border-gray-300 bg-gray-200 dark:border-gray-700"
+          :src="userStore.userInfo?.picture!"
         />
       </div>
+      <div class="mt-4 truncate">
+        <div class="text-3xl font-medium">{{ userStore.userInfo?.username }}</div>
+        <div class="text-md text-gray-400">
+          {{ userStore.userInfo?.name || "未设置" }}
+        </div>
+      </div>
+      <hr class="my-5 dark:border-gray-700" />
+      <!-- TODO: 等后续勋章制作完成再放出来 -->
+      <!-- <div class="text-lg font-medium">勋章</div>
+      <div class="mt-2 grid grid-cols-4 gap-2">
+        <div
+          v-for="i in 6"
+          class="h-16 w-16 rounded-full bg-gray-200 dark:bg-gray-700"
+        ></div>
+      </div> -->
+    </div>
+
+    <!-- 右侧课程包区域 -->
+    <div class="flex-1">
+      <div class="mb-4 flex justify-between border-b pb-2 dark:border-gray-700">
+        <div class="text-xl font-medium">最近使用的课程包</div>
+        <NuxtLink
+          href="/course-pack"
+          class="link text-blue-500 no-underline hover:opacity-75"
+          >更多课程包
+        </NuxtLink>
+      </div>
+      <HomeRecentCoursePack />
+
+      <hr class="my-5 dark:border-gray-700" />
+      <HomeCalendarGraph
+        :data="learnRecord.list"
+        :totalCount="learnRecord.totalCount"
+        @toggleYear="toggleYear"
+      />
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
 import { ref } from "vue";
 
