f4b218860419eb97c95dd494d45dca60b2d83ec7
cuixiaorui <cui_xiaorui@126.com>
2024-05-22 19:08:33 +0800

    style: delete "未设置" when name not set

--- apps/client/components/Home/index.vue
+++ apps/client/components/Home/index.vue
@@ -11,7 +11,7 @@
       <div class="mt-4 truncate">
         <div class="text-3xl font-medium">{{ userStore.userInfo?.username }}</div>
         <div class="text-md text-gray-400">
-          {{ userStore.userInfo?.name || "未设置" }}
+          {{ userStore.userInfo?.name }}
         </div>
       </div>
       <hr class="my-5 dark:border-gray-700" />
