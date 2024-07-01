de421d54b74e9734488d70ffbd1fd2f412630762
cuixiaorui <cui_xiaorui@126.com>
2024-07-01 17:55:52 +0800

    chore: delete course-db project

--- packages/course-db/repositories/statement.ts
+++
@@ -1,8 +0,0 @@
-import { eq } from "drizzle-orm";
-
-import { db } from "@earthworm/db";
-import { statement as statementSchema } from "@earthworm/schema";
-
-export async function deleteStatements(courseId: string) {
-  await db.delete(statementSchema).where(eq(statementSchema.courseId, courseId));
-}
