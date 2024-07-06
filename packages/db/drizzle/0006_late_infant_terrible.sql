163ffa5aea75e346ffde2a68e4b394f096d4decf
cuixiaorui <cui_xiaorui@126.com>
2024-07-06 18:24:06 +0800

    chore: add membership type enum and default value for "type" column

---
+++ packages/db/drizzle/0006_late_infant_terrible.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "memberships" ADD COLUMN "type" "membership_type" DEFAULT 'regular' NOT NULL;
--- packages/schema/src/schema/membership.ts
+++ packages/schema/src/schema/membership.ts
@@ -1,5 +1,8 @@
 import { createId } from "@paralleldrive/cuid2";
-import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+// 定义会员类型的枚举
+const membershipTypeEnum = pgEnum("membership_type", ["regular", "founder"]);
 
 export const membership = pgTable("memberships", {
   id: text("id")
@@ -11,4 +14,5 @@ export const membership = pgTable("memberships", {
   isActive: boolean("isActive").default(true),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").$onUpdateFn(() => new Date()),
+  type: membershipTypeEnum("type").notNull().default("regular"),
 });
