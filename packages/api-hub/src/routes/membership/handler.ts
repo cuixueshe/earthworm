1dba08120ac53cda172923f7ea352a9928621ccc
cuixiaorui <cui_xiaorui@126.com>
2024-07-06 18:29:36 +0800

    chore: Add error logging to checkMembershipHandler

--- packages/api-hub/src/routes/membership/handler.ts
+++ packages/api-hub/src/routes/membership/handler.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
+import { logger } from "../../services/logger";
 import { checkMembership } from "./service";
 
 export async function checkMembershipHandler(
@@ -11,6 +12,7 @@ export async function checkMembershipHandler(
     const membershipStatus = await checkMembership(userId);
     reply.send(membershipStatus);
   } catch (error) {
+    logger.error(error);
     reply.status(500).send({ error: "Internal Server Error" });
   }
 }
