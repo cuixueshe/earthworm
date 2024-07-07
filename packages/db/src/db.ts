ad1ad613d7ea97b3d6e050f4d51d886e8951e08c
cuixiaorui <cui_xiaorui@126.com>
2024-07-07 12:14:39 +0800

    chore(db): delete schema

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,14 +4,10 @@ import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import { schemas } from "@earthworm/schema";
-
 const envName = process.env.NODE_ENV === "prod" ? ".env.prod" : ".env";
 dotenv.config({ path: path.resolve(__dirname, `../../../apps/api/${envName}`) });
 
 console.log("connection string: ", process.env.DATABASE_URL);
 const connection = postgres(process.env.DATABASE_URL ?? "");
 
-export const db = drizzle(connection, {
-  schema: schemas,
-});
+export const db = drizzle(connection);
