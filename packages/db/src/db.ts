5581db398320765081ac97c04291365370d189dc
cuixiaorui <cui_xiaorui@126.com>
2024-07-07 13:20:28 +0800

    chore(db): add type

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+import { schemas } from "@earthworm/schema";
+
 const envName = process.env.NODE_ENV === "prod" ? ".env.prod" : ".env";
 dotenv.config({ path: path.resolve(__dirname, `../../../apps/api/${envName}`) });
 
 console.log("connection string: ", process.env.DATABASE_URL);
 const connection = postgres(process.env.DATABASE_URL ?? "");
 
-export const db = drizzle(connection);
+export const db = drizzle(connection, {
+  schema: schemas,
+});
