23cbcccc76d57674dfaac1e8791aed117ee5fabc
cuixiaorui <cui_xiaorui@126.com>
2024-07-17 17:03:04 +0800

    chore: remove api-hub

--- packages/api-hub/src/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from "fastify";
-
-const root: FastifyPluginAsync = async (fastify) => {
-  fastify.get("/", async function () {
-    return { root: "hi this is api-hub" };
-  });
-};
-
-export default root;
--- packages/api-hub/src/services/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino from "pino";
-
-const isTestEnv = process.env.NODE_ENV === "test";
-export const logger = pino({
-  level: isTestEnv ? "silent" : "info",
-  transport: {
-    target: "pino-pretty",
-  },
-});
